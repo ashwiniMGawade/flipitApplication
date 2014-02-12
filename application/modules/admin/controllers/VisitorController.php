159f44ba60d41fcef69df1247ba9a7c98f7e3500
Surinderpal singh <spsingh1@seasiainfotech.com>
2014-02-12 11:56:00 +0530

    KC-2603 #comment removed search visitor bug on test newsletter page for editor user

--- application/modules/admin/controllers/VisitorController.php
+++ application/modules/admin/controllers/VisitorController.php
@@ -23,11 +23,32 @@ class Admin_VisitorController extends Zend_Controller_Action
 
 		# redirect of a user don't have any permission for this controller
 		$sessionNamespace = new Zend_Session_Namespace();
+		$this->_settings  = $sessionNamespace->settings['rights'] ;
 
-		if($sessionNamespace->settings['rights']['administration']['rights'] != '1' &&
-					$sessionNamespace->settings['rights']['administration']['rights'] != '2' )
-		{
-			$this->_redirect('/admin/auth/index');
+
+		# apply admin level access on all controllers 
+		if($this->getRequest()->isXmlHttpRequest()) {
+		
+			# add action as new case which needs to be viewed by other users
+			switch(strtolower($this->view->action)) {
+				case 'searchemails':
+				 	# no restriction
+				break;
+				default:
+					if( $this->_settings['administration']['rights'] != '1' &&
+						$this->_settings['administration']['rights'] != '2'  ) {
+
+		    			$this->getResponse()->setHttpResponseCode(404);
+   						$this->_helper->redirector('index' , 'index' , null ) ;
+					}
+
+			}
+
+		} else {
+			if($this->_settings['administration']['rights'] != '1' &&
+						$this->_settings['administration']['rights'] != '2' ) 	{
+				$this->_redirect('/admin/auth/index');
+			}
 		}
 	}
 	/**
