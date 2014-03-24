89c21412dc3d179d0134b5d2d6e25e250c624017
Kuldeep Raj <kraj@seasiainfotech.com>
2014-03-24 15:13:37 +0530

    KC-2748 delete helper files

--- application/modules/de/views/helpers/MinifyHeadScript.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once APPLICATION_PATH. '/views/helpers/MinifyHeadScript.php';
-abstract class De_Zend_View_Helper_MinifyHeadScript extends Zend_View_Helper_MinifyHeadScript {
-
-}
-
-?>
