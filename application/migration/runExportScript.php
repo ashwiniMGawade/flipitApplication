65c1aa64ecabc16bdbadf4e6cedb539bdbfa1d77
Kim Pellikaan <kim@web-flight.nl>
2014-03-27 14:25:54 +0100

    Deleted some spaces there.

--- application/migration/runExportScript.php
+++ application/migration/runExportScript.php
@@ -3,21 +3,17 @@
 # run shop export script
 require  'shopExportScript.php';
 
-
 # run offer export script
 require 'offerExportScript.php';
 
-
 # run visitor export script
 require 'visitorExportScript.php';
-
 
 #run global shop  export script
 require 'globalShopExportScript.php';
 
 #run export all shops in associated json file for each locale
 require 'generateAllShopsJsonForSearch.php';
-
 
 #run cretae translation json fiel for each locale (used for javascript translations)
 require 'generateJsonForJsTranlastion.php';
