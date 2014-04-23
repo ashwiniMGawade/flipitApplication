02d5f4024d658acceb8e1ca19a3d336efc814130
Daniel Bakker <info@danielbakker.nl>
2014-04-23 15:04:29 +0200

    Admin option, Action and helper for activation of Inline translate

--- application/controllers/TransController.php
+++ application/controllers/TransController.php
@@ -19,9 +19,30 @@
 			return $this->getHelper('Transl8')->submitAction();
 		}
 
-		public function testAction()
+		public function activateinlinetranslationAction()
 		{
+			$this->_helper->layout->disableLayout();
+			$this->_helper->viewRenderer->setNoRender();
+
+			$session 	= new Zend_Session_Namespace('Transl8');
+			$storeUrl 	= $this->_getParam('storeUrl', 'http://www.flipit.com');
+			$hash 		= $this->_getParam('hash', false);
+
+			if (!empty($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'flipit.com')) {
+				if ($hash == $this->view->inlineTranslationHash() ) {
+					$session->onlineTranslationActivated = true;
+					$this->_redirect( $storeUrl );
+				}else{
+					echo 'Invalid hash, try again from Admin';
+				}
+			}else{
+				$session->onlineTranslationActivated = false;
+				echo "This function can only be activated from the admin";
+			}
 
 		}
-	}
 
+		public function testAction()
+		{
+		}
+	}
