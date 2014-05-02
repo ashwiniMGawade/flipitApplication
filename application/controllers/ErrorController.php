429d075c73f0667a8eec0c12270e4370f17258d9
Kuldeep Raj <kraj@seasiainfotech.com>
2014-05-02 14:11:24 +0530

    change in categories

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -31,7 +31,7 @@ class ErrorController extends Zend_Controller_Action
 
                     $pageLogo  = Logo::getPageLogo($pageDetails['logoId']);
                     $specialPageOffers = Offer::getSpecialPageOffers($pageDetails);
-                    $specialOffersPaginator = FrontEnd_Helper_viewHelper::renderPagination($specialPageOffers, $pageNumber, 5, 3);
+                    $specialOffersPaginator = FrontEnd_Helper_viewHelper::renderPagination($specialPageOffers, $pageNumber, 30, 3);
                     $frontendViewHelper = new FrontEnd_Helper_viewHelper();
                     $sidebarWidget = $frontendViewHelper->getSidebarWidget($sidebarParameters = array(), rtrim($this->pagePermalink, '/'));
 
