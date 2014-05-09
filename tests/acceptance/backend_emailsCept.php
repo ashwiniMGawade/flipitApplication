da4bb4c24b24806c63728a49fe9cd57385a84c3d
Kim Pellikaan <kim@web-flight.nl>
2014-05-09 13:52:48 +0200

    Fixed the test suit as far as possible. Defines still giving problems in the functional tests.

--- tests/acceptance/backend_emailsCept.php
+++ tests/acceptance/backend_emailsCept.php
@@ -2,5 +2,5 @@
 $I = new WebGuy($scenario);
 $I->wantTo('See if the email management is there.');
 TestCommons::logInAdmin($I);
-$I->clickLink('Emails');
-$I->see('Email templates');
+$I->click('Emails');
+$I->see('Emails');
