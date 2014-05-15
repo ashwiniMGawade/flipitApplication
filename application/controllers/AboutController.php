ef7c504d448d6c415b417fc2f66901a6c0dadf3a
Kuldeep Raj <kraj@seasiainfotech.com>
2014-05-15 19:20:41 +0530

    clean rest of code from about controller

--- application/controllers/AboutController.php
+++ application/controllers/AboutController.php
@@ -1,9 +1,6 @@
 <?php
 class AboutController extends Zend_Controller_Action
 {
-    ##########################################################
-    ########### REFACTORED CODE ##############################
-    ##########################################################
     public function indexAction()
     {
         $pageAttributeId = PageAttribute::getPageAttributeIdByName($this->getRequest()->getControllerName());
@@ -71,81 +68,15 @@ class AboutController extends Zend_Controller_Action
         $this->view->sidebarWidgetForm = $signUpFormSidebarWidget;
     }
 
-    ##########################################################
-    ########### END REFACTORED CODE ##########################
-    ##########################################################
-    /**
-     * override views based on modules if exists
-     * @see Zend_Controller_Action::init()
-     * @author Bhart
-     */
     public function init()
     {
         $module   = strtolower($this->getRequest()->getParam('lang'));
         $controller = strtolower($this->getRequest()->getControllerName());
         $action     = strtolower($this->getRequest()->getActionName());
-
-        # check module specific view exists or not
-        if (file_exists (APPLICATION_PATH . '/modules/'  . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml")){
-
-            # set module specific view script path
-            $this->view->setScriptPath( APPLICATION_PATH . '/modules/'  . $module . '/views/scripts' );
-        } else{
-
-            # set default module view script path
-            $this->view->setScriptPath( APPLICATION_PATH . '/views/scripts' );
-        }
-    }
-
-
-    public function clearcacheAction()
-    {
-        $cache = Zend_Registry::get('cache');
-        $cache->clean();
-        echo 'cache is cleared';
-        exit;
-    }
-
-    /**
-     * Find if an email id exists allready for newsletter
-     * @author Raman
-     * @version 1.0
-     */
-    public function checkuserAction()
-    {
-        $u =  new Newslettersub();
-        $cnt  = intval($u->checkDuplicateUser($this->_getParam('email')));
-
-        if($cnt > 0) {
-            echo Zend_Json::encode(false);
-
+        if (file_exists(APPLICATION_PATH . '/modules/'  . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml")) {
+            $this->view->setScriptPath(APPLICATION_PATH . '/modules/'  . $module . '/views/scripts');
         } else {
-
-            echo Zend_Json::encode(true);
+            $this->view->setScriptPath(APPLICATION_PATH . '/views/scripts');
         }
-
-        die();
-    }
-
-    /**
-     * Find if an email id exists allready for newsletter
-     * @author Raman
-     * @version 1.0
-     */
-    public function registerAction()
-    {
-        $u =  new Newslettersub();
-        //echo $this->_getParam('email');
-        //die("Rajajk");
-        $cnt  = intval($u->registerUser($this->_getParam('email')));
-        if($cnt > 0) {
-            echo Zend_Json::encode(false);
-
-        } else {
-
-            echo Zend_Json::encode(true);
-        }
-
-        die();
     }
 }
