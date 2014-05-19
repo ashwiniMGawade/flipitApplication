c1ea483c2fee9ccf82bc544b940501214c6a7208
Kuldeep Raj <kraj@seasiainfotech.com>
2014-05-19 13:15:32 +0530

    KC-3124 done with active class and refactor home.js

--- public/js/front_end/home.js
+++ public/js/front_end/home.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $('.vouchers').hide();
     $('.vouchers:eq(0)').show();
     $('div.tab-content div:gt(0)').hide();
+    removeActiveClass();
 });
 function showRelatedDiv(element){
+	removeActiveClass();
     $('.vouchers').hide();
     $('div#'+$(element).attr('id')).show();
     $('a#'+$(element).attr('id')).addClass('active-li');
@@ -15,4 +17,8 @@ function showTab(element){
     .css({"display":"none","visibility":"hidden"});
     $('div.container ul li').removeClass('active');
     $('a#tab_'+element).parents('li').addClass('active');
+}
+
+function removeActiveClass(){
+    $('.best-items .categories-block ul li a').removeClass('active-li');
 }
