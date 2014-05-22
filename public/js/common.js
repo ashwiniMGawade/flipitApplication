1f284f9f415c91c231950ace90f64b9a3e4d8cda
Kuldeep Raj <kraj@seasiainfotech.com>
2014-05-22 16:52:59 +0530

    KC-3244 #comment remove error in all store page

--- public/js/common.js
+++ public/js/common.js
@@ -120,11 +120,6 @@ function localeSettingCMS(obj)
 }
 
 jQuery(document).ready(function() {
-	//Refactored block
-    if (jQuery(".shop-name")[0]) {
-        jQuery(".shop-name:last-child").append("<a class='btn-top' href='#'>"+ __('Back to top') +"</a>");
-    }
-
 	if(jQuery("div.coupon-code").length > 0)
 	{
 		jQuery("div.coupon-code").bigText({'maximumFontSize': 20});
