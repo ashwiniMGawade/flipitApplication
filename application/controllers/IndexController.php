2dc743eff1d08e55e82fdd6d8fd1c2ea5bc82ef8
Rajbir singh <rsingh6@seasiainfotech.com>
2014-05-29 20:47:45 +0530

    fixed home page

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -17,20 +17,21 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        if (HTTP_HOST != 'www.flipit.com') {
-            $this->view->canonical = '';
-            $this->view->controllerName = $this->getRequest()->getControllerName();
-            $this->view->action = $this->getRequest()->getActionName();
-            $pageAttributeId = Page::getPageAttributeByPermalink($this->getRequest()->getActionName());
-            $pageDetails = Page::getPageFromPageAttribute($pageAttributeId);
-            if (!empty($pageDetails)) {
-                $this->view->pageTitle = ucfirst($pageDetails->pageTitle);
-                $customHeader = isset($pageDetails->customHeader) ? $pageDetails->customHeader : '';
-                $this->viewHelperObject->getFacebookMetaTags($this, $pageDetails->metaTitle, ucfirst(trim($pageDetails->metaTitle)), trim($pageDetails->metaDescription), FrontEnd_Helper_viewHelper::__link($this->getRequest()->getActionName()), FACEBOOK_IMAGE, $customHeader);
-            } else {
-                throw new Zend_Controller_Action_Exception('', 404);
-            }
-            
+        $this->view->canonical = '';
+        $this->view->controllerName = $this->getRequest()->getControllerName();
+        $this->view->action = $this->getRequest()->getActionName();
+        $pageAttributeId = Page::getPageAttributeByPermalink($this->getRequest()->getActionName());
+        $pageDetails = Page::getPageFromPageAttribute($pageAttributeId);
+        if (!empty($pageDetails)) {
+            $this->view->pageTitle = ucfirst($pageDetails->pageTitle);
+            $customHeader = isset($pageDetails->customHeader) ? $pageDetails->customHeader : '';
+            $this->viewHelperObject->getFacebookMetaTags($this, $pageDetails->metaTitle, ucfirst(trim($pageDetails->metaTitle)), trim($pageDetails->metaDescription), FrontEnd_Helper_viewHelper::__link($this->getRequest()->getActionName()), FACEBOOK_IMAGE, $customHeader);
+        } else {
+            throw new Zend_Controller_Action_Exception('', 404);
+        }
+        
+        if (HTTP_HOST == 'www.flipit.com' && $_SERVER['REQUEST_URI'] == '/') {
+        } else {
             $this->view->topOffers = FrontEnd_Helper_viewHelper::getRequestedDataBySetGetCache("all_popularvaouchercode_list", array('function' => 'Offer::getTopOffers', 'parameters' => array(10)));
             $this->view->newOffers = FrontEnd_Helper_viewHelper::getRequestedDataBySetGetCache("all_homenewoffer_list", array('function' => 'Offer::getNewestOffers', 'parameters' => array('newest', 10)));
             $topCategories = FrontEnd_Helper_viewHelper::getRequestedDataBySetGetCache("all_popularcategory_list", array('function' => 'Category::getPopularCategories', 'parameters' => array(10)));
