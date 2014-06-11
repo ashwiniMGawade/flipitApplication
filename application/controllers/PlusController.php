27bc67c6a1373a8928d5771fdbc6d95d81758092
Kuldeep Raj <kraj@seasiainfotech.com>
2014-06-11 18:10:05 +0530

    KC-3209 #comment Clean controller and views

--- application/controllers/PlusController.php
+++ application/controllers/PlusController.php
@@ -6,12 +6,14 @@ class PlusController extends Zend_Controller_Action
         $module   = strtolower($this->getRequest()->getParam('lang'));
         $controller = strtolower($this->getRequest()->getControllerName());
         $action     = strtolower($this->getRequest()->getActionName());
-
-        if (file_exists(
-            APPLICATION_PATH . '/modules/'  . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml")){
-            $this->view->setScriptPath( APPLICATION_PATH . '/modules/'  . $module . '/views/scripts' );
+        if (
+            file_exists(
+                APPLICATION_PATH . '/modules/'  . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml"
+            )
+        ) {
+            $this->view->setScriptPath(APPLICATION_PATH . '/modules/'  . $module . '/views/scripts');
         } else {
-            $this->view->setScriptPath( APPLICATION_PATH . '/views/scripts' );
+            $this->view->setScriptPath(APPLICATION_PATH . '/views/scripts');
         }
         $this->viewHelperObject = new FrontEnd_Helper_viewHelper();
     }
@@ -32,13 +34,28 @@ class PlusController extends Zend_Controller_Action
             ['pageTitle'] :'';
         $this->view->permaLink = $articleOverviewPagePermalink;
         $this->view->canonical = FrontEnd_Helper_viewHelper::generateCononical($articleOverviewPagePermalink);
-        $customHeader = isset($articleOverviewPageDetails[0]['customHeader']) ? 
-            $articleOverviewPageDetails[0]['customHeader'] : '';
-        $this->viewHelperObject->getMetaTags($this, isset($articleOverviewPageDetails[0]['pageTitle']) ?
-        $articleOverviewPageDetails[0]['pageTitle'] :'', trim(isset($articleOverviewPageDetails[0]['metaTitle']) ?
-        $articleOverviewPageDetails[0]['metaTitle'] :''), trim(isset($articleOverviewPageDetails[0]['metaDescription']) 
-            ? $articleOverviewPageDetails[0]['metaDescription'] :''), $articleOverviewPagePermalink,
-                HTTP_PATH."public/images/plus_og.png", $customHeader);
+        $customHeader =
+            isset($articleOverviewPageDetails[0]['customHeader'])
+            ? $articleOverviewPageDetails[0]['customHeader']
+            : '';
+        $this->viewHelperObject->getMetaTags(
+            $this,
+            isset($articleOverviewPageDetails[0]['pageTitle'])
+            ? $articleOverviewPageDetails[0]['pageTitle'] :'',
+            trim(
+                isset($articleOverviewPageDetails[0]['metaTitle'])
+                ? $articleOverviewPageDetails[0]['metaTitle']
+                :''
+            ),
+            trim(
+                isset($articleOverviewPageDetails[0]['metaDescription'])
+                ? $articleOverviewPageDetails[0]['metaDescription']
+                :''
+            ),
+            $articleOverviewPagePermalink,
+            HTTP_PATH."public/images/plus_og.png",
+            $customHeader
+        );
 
         $this->view->pageDetails = $articleOverviewPageDetails;
         $this->view->mostReadArticles = $mostReadArticles;
@@ -53,14 +70,16 @@ class PlusController extends Zend_Controller_Action
         $articleDetails = Articles::getArticleByPermalink($this->getRequest()->getParam('permalink'));
         $currentArticleCategory = $articleDetails[0]['relatedcategory'][0]['articlecategory']['name'];
         $categoryWiseArticles = MoneySaving::getCategoryWiseArticles();
-        $articlesRelatedToCurrentCategory = !empty($categoryWiseArticles[$currentArticleCategory]) ? 
-            $categoryWiseArticles[$currentArticleCategory] : '';
+        $articlesRelatedToCurrentCategory =
+            !empty($categoryWiseArticles[$currentArticleCategory])
+            ? $categoryWiseArticles[$currentArticleCategory]
+            : '';
         $incrementArticleViewCountValue  = FrontEnd_Helper_viewHelper::
             viewCounter('article', 'onload', $articleDetails[0]['id']);
             
         if (!empty($articleDetails)) {
-            $this->view->canonical = FrontEnd_Helper_viewHelper::generateCononical(
-                $this->getRequest()->getParam('permalink'));
+            $this->view->canonical =
+                FrontEnd_Helper_viewHelper::generateCononical($this->getRequest()->getParam('permalink'));
             $this->view->mostReadArticles = FrontEnd_Helper_viewHelper::
                 getRequestedDataBySetGetCache("all_mostreadMsArticlePage_list", array(
                     'function' => 'MoneySaving::getMostReadArticles', 'parameters' => array(3)));
@@ -70,9 +89,15 @@ class PlusController extends Zend_Controller_Action
             $this->view->topPopularOffers = Offer::getTopOffers(5);
             $this->view->userDetails =  User::getProfileImage($articleDetails[0]['authorid']);
           
-            $this->viewHelperObject->getMetaTags($this, $articleDetails[0]['title'], 
-                trim($articleDetails[0]['metatitle']), trim($articleDetails[0]['metadescription']), 
-                    $articleDetails[0]['permalink'], FACEBOOK_IMAGE, '');
+            $this->viewHelperObject->getMetaTags(
+                $this,
+                $articleDetails[0]['title'],
+                trim($articleDetails[0]['metatitle']),
+                trim($articleDetails[0]['metadescription']),
+                $articleDetails[0]['permalink'],
+                FACEBOOK_IMAGE,
+                ''
+            );
             $this->view->pageCssClass = 'in-savings-page author-page';
         } else {
               throw new Zend_Controller_Action_Exception('', 404);
