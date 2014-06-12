61cd174274e58607c3337aeb5f017fecd85787ba
Kuldeep Raj <kraj@seasiainfotech.com>
2014-06-12 12:49:31 +0530

    KC-3209 #comment cleand login controller and create a helper for login controller

--- application/controllers/ViewcountController.php
+++ application/controllers/ViewcountController.php
@@ -1,23 +1,6 @@
 <?php
 class ViewcountController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-        $module   = strtolower($this->getRequest()->getParam('lang'));
-        $controller = strtolower($this->getRequest()->getControllerName());
-        $action     = strtolower($this->getRequest()->getActionName());
-        if (
-            file_exists(
-                APPLICATION_PATH . '/modules/' . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml"
-            )
-        ) {
-            $this->view->setScriptPath(APPLICATION_PATH . '/modules/' . $module . '/views/scripts');
-        } else {
-            $this->view->setScriptPath(APPLICATION_PATH . '/views/scripts');
-        }
-    }
-
     public function storecountAction()
     {
         $type = $this->_getParam('type');
