bf4b7cccc42acaa0420210375c3d977d58fdf3ba
Amit Sharma <asharma11@seasiainfotech.com>
2014-06-18 13:00:18 +0530

    KC-3470 #comment fixed contact us page form

--- application/controllers/ContactController.php
+++ application/controllers/ContactController.php
@@ -6,7 +6,42 @@ class ContactController extends Zend_Controller_Action
     public function getcontactformdetailsAction()
     {
         $parameters = $this->_getAllParams();
-        echo "<pre>";print_r($parameters);die;
+        $visitorName = $parameters['name'];
+        $visitorEmail = FrontEnd_Helper_viewHelper::sanitize($parameters['email']);
+        $subject = $parameters['subject'];
+        $message = $parameters['message'];
 
+        self::sendMailThroughMandril($visitorName, $visitorEmail, $subject, $message);
+    }
+
+    public function sendMailThroughMandril($visitorName, $visitorEmail, $subject, $message)
+    {
+        $adminEmail = "asharma11@seasiainfotech.com";
+        if (!empty($visitorEmail)) {
+            $mailer  = new FrontEnd_Helper_Mailer();
+            $content = array(
+                'name'    => 'content',
+                'content' => $this->view->partial(
+                    'emails/contactform.phtml',
+                    array(
+                        'message' => $message
+                    )
+                )
+            );
+            $mailer->send(
+                FrontEnd_Helper_viewHelper::__email('email_sitename'),
+                $visitorEmail,
+                $visitorName,
+                $adminEmail,
+                FrontEnd_Helper_viewHelper::__email('email_'.$subject),
+                $content,
+                FrontEnd_Helper_viewHelper::__email('email_Forgot password header')
+            );
+        }
+        $successMessage = "Your message has been sent.";
+        $flashMessage = $this->_helper->getHelper('FlashMessenger');
+        $flashMessage->addMessage(array('success' => $successMessage));
+        $urlToRedirect = HTTP_PATH_LOCALE.'info/contact';
+        $this->_redirect($urlToRedirect);
     }
 }
