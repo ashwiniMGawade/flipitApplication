74fdb6b51e69d7d14f57a569988332ee78f86f44
Kim Pellikaan <kim@web-flight.nl>
2014-06-30 23:15:24 +0200

    Created a global Csv writer/ reader.

---
+++ application/services/CsvWriter.php
@@ -0,0 +1,19 @@
+<?php
+class Application_Service_CsvWriter extends Application_Service_CsvBase
+{
+    public function writeRow($row)
+    {
+        if (is_string($row)) {
+            $row = explode(',', $row);
+            $row = array_map('trim', $row);
+        }
+        return $this->handle->fputcsv($row, $this->delimiter, $this->enclosure);
+    }
+
+    public function writeFromArray(array $array)
+    {
+        foreach ($array as $key => $value) {
+            $this->writeRow($value);
+        }
+    }
+}
