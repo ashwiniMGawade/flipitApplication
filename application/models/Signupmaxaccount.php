d1ac4269ca675592ba0b66bde33cebf8e60d105f
Rajbir singh <rsingh6@seasiainfotech.com>
2014-07-10 19:09:34 +0530

    fixed locale signupmaxaccount

--- application/models/Signupmaxaccount.php
+++ application/models/Signupmaxaccount.php
@@ -222,7 +222,8 @@ class Signupmaxaccount extends BaseSignupmaxaccount
             'p.id,p.no_of_acc,p.max_account,p.status,p.email_confirmation,p.email_header,p.email_footer,
             p.emailperlocale,p.sendername,p.emailsubject,p.testemail,p.showTestimonial,p.testimonial1,
             p.testimonial2,p.testimonial3,p.homepagebanner_path,p.homepagebanner_name,p.homepage_widget_banner_path,
-            p.homepage_widget_banner_name,p.newletter_is_scheduled,p.newletter_status,p.newletter_scheduled_time'
+            p.homepage_widget_banner_name,p.newletter_is_scheduled,p.newletter_status,p.newletter_scheduled_time,
+            p.locale,p.timezone'
         )
         ->from('Signupmaxaccount p')
         ->where('id=1')
