01e5f5e4a15437764b805e987ccb77c09c94829c
Kim Pellikaan <kim@web-flight.nl>
2014-07-22 10:12:08 +0200

    brought the frontend_php translations files to the fallback folder

--- application/migration/offerExportScript.php
+++ application/migration/offerExportScript.php
@@ -90,7 +90,7 @@ class OfferExport
 
         $this->_trans->addTranslation(
                 array(
-                        'content' => APPLICATION_PATH.'/../public/'. strtolower($this->_localePath).'language/frontend_php' . $suffix . '.mo',
+                        'content' => APPLICATION_PATH.'/../public/'. strtolower($this->_localePath).'language/fallback/frontend_php' . $suffix . '.mo',
                         'locale' => $cutsomLocale,
                 )
         );
