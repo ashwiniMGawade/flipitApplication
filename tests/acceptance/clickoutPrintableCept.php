5bbfaeb04f5a78e8f3b3249bb0c021a62b1e6939
Rajbir singh <rsingh6@seasiainfotech.com>
2014-08-13 10:44:20 +0530

    KC-3906 worked on clickouts test

--- tests/acceptance/clickoutPrintableCept.php
+++ tests/acceptance/clickoutPrintableCept.php
@@ -3,7 +3,7 @@ $I = new AcceptanceTester($scenario);
 $I->amOnPage('/in/acceptance-shop');
 $I->click('Click to View Information');
 $I->seeInCurrentUrl('/in');
-$I->wait(10);
+$I->wait(5);
 $I->amOnPage('/in/acceptance-shop');
 $I->click('.btn-print');
 $I->executeInSelenium(function (\Webdriver $webdriver) {
@@ -11,5 +11,20 @@ $I->executeInSelenium(function (\Webdriver $webdriver) {
     $last_window = end($handles);
     $webdriver->switchTo()->window($last_window);
 });
-$I->wait(10);
+$I->wait(5);
 $I->seeInCurrentUrl('/in');
+$I->wait(5);
+$I->switchToWindow();
+
+$I->wait(5);
+$I->amOnPage('/in/acceptance-shop');
+$I->click('.clickout-title a');
+$I->wait(5);
+$I->seeInCurrentUrl('/in');
+$I->wait(5);
+
+$I->amOnPage('/in/acceptance-shop');
+$I->click('img[alt=printable]');
+$I->wait(5);
+$I->seeInCurrentUrl('/in');
+$I->wait(5);
--- tests/acceptance/clickoutSaleCept.php
+++ tests/acceptance/clickoutSaleCept.php
@@ -9,3 +9,16 @@ $I->executeInSelenium(function (\Webdriver $webdriver) {
 });
 $I->wait(10);
 $I->seeInCurrentUrl('/in');
+
+$I->wait(5);
+$I->amOnPage('/in/acceptance-shop');
+$I->click('.clickout-title a');
+$I->wait(5);
+$I->seeInCurrentUrl('/in');
+$I->wait(5);
+
+$I->amOnPage('/in/acceptance-shop');
+$I->click('.small-code');
+$I->wait(5);
+$I->seeInCurrentUrl('/in');
+$I->wait(5);
