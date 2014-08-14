3ed32d6ee9928aa85e91811c0332d1b6905b98cc
Amit Sharma <asharma11@seasiainfotech.com>
2014-08-14 11:36:54 +0530

    KC-4031 #fixed issues after review

--- library/FrontEnd/Helper/MoneySavingGuidesPartialFunctions.php
+++ library/FrontEnd/Helper/MoneySavingGuidesPartialFunctions.php
@@ -16,7 +16,9 @@ class FrontEnd_Helper_MoneySavingGuidesPartialFunctions
             $articleTitle = mb_strlen($article['title']) > 50 ?
                                         mb_substr($article['title'], 0, 50).'..' : $article['title'];
             $articleBy = $authorName != '' ? FrontEnd_Helper_viewHelper::__translate('By') : '';
-            $categoryTitleBackgroundColor = $article['articlecategory'][0]['categorytitlecolor'];
+            $categoryTitleBackgroundColor = isset($article['articlecategory'][0]['categorytitlecolor'])
+                                                ? $article['articlecategory'][0]['categorytitlecolor']
+                                                : 'e69342';
             $relatedArticles .=
                     '<article class="article col-md-3 col-sm-4 col-xs-6 ">
                         <div class="image">
