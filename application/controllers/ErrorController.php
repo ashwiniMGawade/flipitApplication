fb0dc2d386e0eeb1ae5d19287921d370729f5c03
Amit Sharma <asharma11@seasiainfotech.com>
2014-08-27 17:57:24 +0530

    KC-3285 #fixed special pages error

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -159,23 +159,6 @@ class ErrorController extends Zend_Controller_Action
         } else {
             $pagedata= '';
         }
-        
-        $this->pagePermalink = $pagePermalink;
-        $positionOfSpecialCharactetr = strpos($pagePermalink, "-");
-        if ($positionOfSpecialCharactetr) {
-            $stringWithoutSpecilaChracter = str_replace("-", "", $pagePermalink);
-                $cacheKey = $stringWithoutSpecilaChracter;
-        } else {
-            $cacheKey = $pagePermalink;
-        }
-        
-        $pagedata = FrontEnd_Helper_viewHelper::
-            getRequestedDataBySetGetCache(
-                'page_'.$cacheKey.'_data',
-                array(
-                    'function' => 'Page::getPageDetailsInError', 'parameters' => array(rtrim($pagePermalink, '/'))
-                )
-            );
         return $pagedata;
     }
 }
