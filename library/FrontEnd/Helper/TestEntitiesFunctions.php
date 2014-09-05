57e2094ccf3eb0ba9e4e8ab50b9195494aa75cd7
Kuldeep Raj <kraj@seasiainfotech.com>
2014-09-05 17:30:21 +0530

    Done with configuration

--- library/FrontEnd/Helper/TestEntitiesFunctions.php
+++ library/FrontEnd/Helper/TestEntitiesFunctions.php
@@ -1,11 +1,12 @@
 <?php
 class FrontEnd_Helper_TestEntitiesFunctions
 {
-    public $entityManager = array();
-
+    public $entityManagerUser = array();
+    public $entityManagerUserLocale = array();
     public function __construct()
     {
-        $this->entityManager = \Zend_Registry::get('emUser');
+        $this->entityManagerUser = \Zend_Registry::get('emUser');
+        $this->entityManagerUserLocale =\Zend_Registry::get('emLocale');
     }
     
     public function saveUser($profileId, $websiteId)
@@ -14,30 +15,35 @@ class FrontEnd_Helper_TestEntitiesFunctions
         $user->firstname = 'Test';
         $user->lastname = "By Dev";
         $user->mainText = 'aasdasd';
-        $user->profileimage = $this->entityManager->find('KC\Entity\ProfileImage', $profileId);
+        $user->profileimage = $this->entityManagerUser->find('KC\Entity\ProfileImage', $profileId);
         $user->deleted = 0;
         $user->addtosearch = '0';
-        $user->website[] = $this->entityManager->find('KC\Entity\Website', $websiteId);
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        $user->website[] = $this->entityManagerUser->find('KC\Entity\Website', $websiteId);
+        $this->entityManagerUser->persist($user);
+        $this->entityManagerUser->flush();
         return 'Record has been successfully saved! - '.$user->id;
     }
 
     public function getUser($userId)
     {
-        $user = $this->entityManager->find('KC\Entity\User', $userId);
+        $user = $this->entityManagerUser->find('KC\Entity\User', $userId);
         return $user;
     }
 
     public function getUserWithRelation($userId)
     {
-        $entityManager = $this->entityManager->createQueryBuilder();
-        $query = $entityManager->select('user.firstname, website.name')
+        $entityManagerUser = $this->entityManagerUser->createQueryBuilder();
+        $query = $entityManagerUser->select('user.firstname, website.name')
             ->from('KC\Entity\User', 'user')
             ->leftJoin('user.website', 'website')
             ->setParameter(1, $userId)
             ->where('user.id = ?1');
         $userDetails = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $userDetails;
+    }
+
+    public function get($varnishId = 35891)
+    {
+        return $this->entityManagerUserLocale->find('KC\Entity\Varnish', $varnishId);
     }
 }
