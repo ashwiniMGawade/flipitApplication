6569ffa82dad86b3b8ce707f4ce06eca4fe13490
Kim Pellikaan <kim@web-flight.nl>
2014-09-09 17:43:09 +0200

    KC-4348 #comment Added google tag manager code to the footer.

--- library/FrontEnd/Helper/LayoutContent.php
+++ library/FrontEnd/Helper/LayoutContent.php
@@ -128,6 +128,21 @@ class FrontEnd_Helper_LayoutContent
         return $googleAnalyticsCode;
     }
 
+    public static function loadGoogleTagManager()
+    {
+        $googleTagManager = "<!-- Google Tag Manager -->
+            <noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-W87MZ3\"
+            height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W87MZ3');</script>
+            <!-- End Google Tag Manager -->";
+
+        return $googleTagManager;
+    }
+
     public static function loadFavicon()
     {
         $favicon = '';
