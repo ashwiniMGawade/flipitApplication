ad48dc47c6a29292a0daadc33ed9434f5bc001e1
Kuldeep Raj <kraj@seasiainfotech.com>
2014-09-17 15:37:37 +0530

    KC-4502 : change buttons as per psd

--- library/FrontEnd/Helper/ShopHeaderPartialFunctions.php
+++ library/FrontEnd/Helper/ShopHeaderPartialFunctions.php
@@ -90,7 +90,7 @@ class FrontEnd_Helper_ShopHeaderPartialFunctions extends FrontEnd_Helper_viewHel
         $titleTextForLove = $this->__form('form_Remove from Favourite');
         $emAndSpan = '<em>'.$this->__translate("I FOLLOW THIS").'</em>
             <strong>'.$this->__translate("REMOVE").'</strong>';
-        $aTagClasses = 'pop btn btn-sm btn-default remove-fovourite';
+        $aTagClasses = 'pop btn btn-sm btn-default remove-fovourite blue';
         if (Visitor::getFavoriteShopsForUser($visitorId, $shopId)==false):
             $titleTextForLove = $this->__form("form_Add in Favourite");
             $emAndSpan = '<span class="glyphicon red-heart glyphicon-heart"></span>
