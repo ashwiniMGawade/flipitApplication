57035256e00bb152f2c8a1a103feeea9eacbe214
Kuldeep Raj <kraj@seasiainfotech.com>
2014-09-26 15:38:49 +0530

    KC-4435 : menu model

--- library/KC/Entity/Menu.php
+++ library/KC/Entity/Menu.php
@@ -60,17 +60,24 @@ class Menu
      */
     private $position;
 
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->$property = $value;
+    }
     ######################## Refactored #############################
     public static function replaceBespaarwijzerWithPlus()
     {
-        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
-        $query = $queryBuilder->update('KC\Entity\Menu', 'm')
-                ->set('m.name', 'plus')
-                ->set('m.url', 'plus')
-                ->setParameter(1, 46)
-                ->where('m.id = ?1')
-                ->getQuery();
-        $query->execute();
+        $entityManagerLocale  =\Zend_Registry::get('emLocale');
+        $menu =  $entityManagerLocale->find('KC\Entity\Menu', 46);
+        $menu->name = 'plus';
+        $menu->url = 'plus';
+        $entityManagerLocale->persist($menu);
+        $entityManagerLocale->flush();
         return true;
     }
 
@@ -84,26 +91,26 @@ class Menu
     }
     ######################## Refactored #############################
 
-    public static function getAllTree()
-    {
-        $treeObject     = Doctrine_Core::getTable('mainmenu')->getTree();
-        return $treeObject;
-    }
-  
     public static function insertOne($params)
     {
+        $entityManagerLocale = \Zend_Registry::get('emLocale');
         if ($params['hidimage']!='') {
-            $image = new Image();
+            $image = new KC\Entity\Image();
             $image->path = $params['hidimage'];
-            $image->name = BackEnd_Helper_viewHelper::stripSlashesFromString($params['hidimageorg']);
+            $image->name = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['hidimageorg']);
             $image->type = 'menuIcon';
-            $image->save();
+            $image->created_at = new \DateTime('now');
+            $image->updated_at = new \DateTime('now');
+            $image->deleted = '0';
+            $entityManagerLocale->persist($image);
+            $entityManagerLocale->flush();
         }
 
-        $menu = new menu();
-        $menu->name = BackEnd_Helper_viewHelper::stripSlashesFromString(htmlentities($params['label'], ENT_QUOTES, 'UTF-8'));
-        $menu->url = BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
-        $menu->parentId=0;
+        $menu = new Menu();
+        $menu->name = \BackEnd_Helper_viewHelper::stripSlashesFromString(htmlentities($params['label'], ENT_QUOTES, 'UTF-8'));
+        $menu->url = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
+        $menu->parentId = 0;
+
         if ($params['position']!='') {
             $menu->position = $params['position'];
         }
@@ -113,40 +120,53 @@ class Menu
         if ($params['hidimage']!='') {
             $menu->iconId = $image->id;
         }
-        $menu->save();
+        
+        $entityManagerLocale->persist($menu);
+        $entityManagerLocale->flush();
 
         if ($params['url']!='') {
-            $getRecord = Doctrine_Core::getTable('RoutePermalink')
-            ->findOneBy('exactlink', $params['url']);
+            $repo = $entityManagerLocale->getRepository('KC\Entity\RoutePermalink');
+            $getRecord = $repo->findOneBy(array('exactlink' => $params['url']));
             if (!empty($getRecord) > 0) {
-                $del = Doctrine_Query::create()->delete('RoutePermalink')->where('exactlink = "'. $params['url'].'"')->execute();
+                $repo = $entityManagerLocale->getRepository('KC\Entity\RoutePermalink');
+                $routePermalink = $repo->findBy(array('exactlink' =>  $params['url']));
+                $entityManagerLocale->remove($routePermalink);
+                $entityManagerLocale->flush();
             }
-
+            
+            $entityManagerLocale = \Zend_Registry::get('emLocale');
             $route = new RoutePermalink();
-            $route->permalink = BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
+            $route->permalink = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
             $route->type = 'PG';
-            $route->exactlink = BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
-            $route->save();
+            $route->created_at = new \DateTime('now');
+            $route->updated_at = new \DateTime('now');
+            $route->deleted = '0';
+            $route->exactlink = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
+            $entityManagerLocale->persist($route);
+            $entityManagerLocale->flush();
         }
-
-        $treeObject = Doctrine_Core::getTable('menu')->getTree();
-        $treeObject->createRoot($menu);
         return $params;
     }
      
     public static function insertNode($params)
     {
+        $entityManagerLocale = \Zend_Registry::get('emLocale');
         if ($params['hidimage']!='') {
-            $image = new Image();
+            $image = new KC\Entity\Image();
             $image->path = $params['hidimage'];
             $image->name = $params['hidimageorg'];
             $image->type = 'menuIcon';
-            $image->save();
+            $image->created_at = new \DateTime('now');
+            $image->updated_at = new \DateTime('now');
+            $entityManagerLocale->persist($image);
+            $entityManagerLocale->flush();
         }
 
-        $child1 = new menu();
-        $child1->name = BackEnd_Helper_viewHelper::stripSlashesFromString(htmlentities($params['label'], ENT_QUOTES, 'UTF-8'));
-        $child1->url = BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
+        $child1 = new Menu();
+        $child1->name = \BackEnd_Helper_viewHelper::stripSlashesFromString(
+            htmlentities($params['label'], ENT_QUOTES, 'UTF-8')
+        );
+        $child1->url = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
         $child1->position = $params['position'];
         $child1->parentId = $params['hid'];
 
@@ -154,8 +174,8 @@ class Menu
             $child1->iconId = $image->id;
         }
 
-        $menu = Doctrine_Core::getTable('menu')->findOneById($params['hid']);
-        $child1->getNode()->insertAsLastChildOf($menu);
+        $entityManagerLocale->persist($child1);
+        $entityManagerLocale->flush();
         if ($params['roothid']!='') {
             return $params['roothid'];
         } else {
@@ -165,101 +185,50 @@ class Menu
 
     public static function deleteOne()
     {
-        $menu = Doctrine_Core::getTable('menu')->findOneByName('Child menu 1');
-        $menu->getNode()->delete();
+        $repo = $entityManagerLocale->getRepository('KC\Entity\Menu');
+        $menu = $repo->findOneBy(array('Name' =>  'Child menu 1'));
+        $entityManagerLocale->remove($menu);
+        $entityManagerLocale->flush();
     }
 
-    public static function moveOne()
-    {
-        $menu = new menu();
-        $menu->name = 'Root menu 2';
-        $menu->save();
-
-        $menuTable = Doctrine_Core::getTable('menu');
-        $treeObject    = $menuTable->getTree();
-        $treeObject->createRoot($menu);
-
-        $childmenu = $menuTable->findOneByName('Child menu 1');
-        $childmenu->getNode()->moveAsLastChildOf($menu);
-    }
-
-    public static function getNodeType()
-    {
-        $menu = Doctrine_Core::getTable('menu')->findOneByName('Child menu 1');
-        $isLeaf = $menu->getNode()->isLeaf();
-        $isRoot = $menu->getNode()->isRoot();
-    }
-
-    public static function getSiblings()
-    {
-        $menu = Doctrine_Core::getTable('menu')->findOneByName('Child menu 1');
-        $hasNextSib = $menu->getNode()->hasNextSibling();
-        $hasPrevSib = $menu->getNode()->hasPrevSibling();
-    }
-
-    public static function getInTreeFormat()
-    {
-        $q = Doctrine_Query::create()
-        ->select('c.name, p.name, m.name')
-        ->from('menu c')
-        ->setHydrationMode(Doctrine_Core::HYDRATE_ARRAY);
-        $treeObject = Doctrine_Core::getTable('menu')->getTree();
-        $treeObject->setBaseQuery($q);
-        $tree       = $treeObject->fetchTree();
-        $treeObject->resetBaseQuery();
-    }
-
-    public static function getWithIndention()
-    {
-        $array = array();
-        $treeObject     = Doctrine_Core::getTable('menu')->getTree();
-        $rootColumnName = $treeObject->getAttribute('rootColumnName');
-        foreach ($treeObject->fetchRoots() as $root) {
-            $options = array(
-                    'root_id' => $root->$rootColumnName
-            );
-            foreach ($treeObject->fetchTree($options) as $node) {
-                $array .= str_repeat(' ', $node['level']) . $node['name'];
-            }
-
-        }
-        $data = self::getarray($array);
-    }
-    
     public static function getMainMenuList()
     {
-            $menuList = Doctrine_Query::create()
-            ->select('m.*')
-            ->from("menu m")
-            ->orderBy("m.position")
-            ->fetchArray();
-            return $menuList;
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder->select('menu')
+            ->from('KC\Entity\Menu', 'menu')
+            ->orderBy('menu.position', 'ASC');
+        $menuList = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+        return $menuList;
     }
     
     public static function getmenuList()
     {
-        $menuList = Doctrine_Query::create()
-            ->select('m.*')
-            ->from("menu m")
-            ->where("level=0")
-            ->orderBy("m.position asc")
-            ->fetchArray();
-         return $menuList;
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder->select('menu')
+            ->from('KC\Entity\Menu', 'menu')
+            ->setParameter(1, '0')
+            ->where('menu.level = ?1')
+            ->orderBy('menu.position', 'ASC');
+        $menuList = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+        return $menuList;
+
     }
 
     public static function getmenuRecord($id)
     {
-        $data = Doctrine_Query::create()->select("m.*,i.path,i.name")
-        ->from('menu m')
-        ->leftJoin("m.menuIcon i")
-        ->where("m.id =". $id)
-        ->fetcharray(null, Doctrine::HYDRATE_ARRAY);
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder->select('m.*, i.path, i.name')
+            ->from('KC\Entity\Menu', 'm')
+            ->leftJoin("m.menuIcon", "i")
+            ->setParameter(1, $id)
+            ->where('m.id = ?1');
+        $data = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $data;
-
     }
 
     public static function editmenuRecord($params)
     {
+        $entityManagerLocale  =\Zend_Registry::get('emLocale');
         if ($params['position']=='' || $params['position']==0) {
             $position = '1';
         } else {
@@ -268,84 +237,87 @@ class Menu
         $imageid = null;
         if ($params['imageid']!='') {
             $imageid = $params['imageid'];
-            $image = Doctrine_Query::create()
-            ->update('Image i')
-            ->set('i.path', '"'.$params['hidimage'].'"')
-            ->set('i.name', '"'.$params['hidimageorg'].'"')
-            ->where('i.id='.$params['imageid']);
-            $image->execute();
+            $image =  $entityManagerLocale->find('KC\Entity\Image', $params['imageid']);
+            $image->path = $params['hidimage'];
+            $image->name = $params['hidimageorg'];
+            $entityManagerLocale->persist($image);
+            $entityManagerLocale->flush();
         }
 
         if ($params['imageid']=='' && $params['hidimage']!='') {
-            $image = new Image();
+            $image = new KC\Entity\Image();
             $image->path = '"'.$params['hidimage'].'"';
             $image->name = '"'.$params['hidimageorg'].'"';
             $image->type = 'menuIcon';
-            $image->save();
+            $image->created_at = new \DateTime('now');
+            $image->updated_at = new \DateTime('now');
+            $entityManagerLocale->persist($image);
+            $entityManagerLocale->flush();
             $imageid = $image->id;
         }
 
-        $data = Doctrine_Query::create()
-        ->update('menu m')
-        ->set('m.name', "'". BackEnd_Helper_viewHelper::stripSlashesFromString(htmlentities($params['label'], ENT_QUOTES, 'UTF-8'))."'")
-        ->set('m.url', "'". BackEnd_Helper_viewHelper::stripSlashesFromString($params['url'])."'")
-        ->set('m.position', $position)
-        ->set('m.iconId', '"'.$imageid.'"')
-        ->where('m.id='.'"'.$params['hid'].'"');
-        $data->execute();
+        $menu =  $entityManagerLocale->find('KC\Entity\Menu', $params['hid']);
+        $menu->name = \BackEnd_Helper_viewHelper::stripSlashesFromString(htmlentities($params['label'], ENT_QUOTES, 'UTF-8'));
+        $menu->url = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['url']);
+        $menu->position = $position;
+        $menu->iconId = $imageid;
+        $entityManagerLocale->persist($menu);
+        $entityManagerLocale->flush();
         return $params;
     }
 
     public static function getrtmenuRecord($id)
     {
-        $menuList = Doctrine_Query::create()
-        ->select('m.*')
-        ->from("menu m")
-        ->where('m.root_id='.$id)
-        ->orderBy('m.level ASC')
-        ->addOrderBy('m.position')
-        ->fetchArray();
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder->select('m')
+            ->from('KC\Entity\Menu', 'm')
+            ->setParameter(1, $id)
+            ->where('m.root_id = ?1')
+            ->orderBy('m.level', 'ASC')
+            ->addOrderBy('m.position', 'ASC');
+        $menuList = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $menuList;
     }
     
     public static function gethighposition()
     {
-        $menuList = Doctrine_Query::create()
-        ->select('m.*,MIN(position)')
-        ->from("menu m")
-        ->where('m.level=0')
-        ->fetchArray();
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder->select('m, MIN(m.position)')
+            ->from('KC\Entity\Menu', 'm')
+            ->setParameter(1, '0')
+            ->where('m.level = ?1');
+        $menuList = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $menuList;
     }
 
     public static function deleteMenuRecord($params)
     {
-        $del = Doctrine_Query::create()->delete()
-            ->from('menu m')
-                ->where("m.id=".@$params['id'])
-            ->execute();
+        $entityManagerLocale  =\Zend_Registry::get('emLocale');
+        $menu =  $entityManagerLocale->find('KC\Entity\Menu', $id);
+        $entityManagerLocale->remove($menu);
+        $entityManagerLocale->flush();
         if ($params['parentId']==0 || $params['parentId']==null) {
-            $del = Doctrine_Query::create()->delete()
-                ->from('menu m')
-                ->where("m.root_id=".@$params['id'])
-                ->execute();
+                $repo = $entityManagerLocale->getRepository('KC\Entity\Menu');
+                $menu = $repo->findOneBy(array('root_id' => @$params['id']));
+                $entityManagerLocale->remove($menu);
+                $entityManagerLocale->flush();
         }
 
         if ($params['parentId']!=0 && $params['parentId']!=null) {
-            $del = Doctrine_Query::create()->delete()
-            ->from('menu m')
-            ->where("m.parentId=".@$params['id'])
-            ->execute();
+            $repo = $entityManagerLocale->getRepository('KC\Entity\Menu');
+            $menu = $repo->findOneBy(array('parentId' => @$params['id']));
+            $entityManagerLocale->remove($menu);
+            $entityManagerLocale->flush();
         }
-
         return true;
     }
 
     public static function deleteAllMenuRecord()
     {
-        $del = Doctrine_Query::create()->delete()
-        ->from('menu m')
-        ->execute();
+        $entityManagerLocale  =\Zend_Registry::get('emLocale');
+        $menu =  $entityManagerLocale->getRepository('KC\Entity\Menu');
+        $entityManagerLocale->remove($menu);
+        $entityManagerLocale->flush();
         return true;
     }
 
