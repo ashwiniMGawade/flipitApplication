296d1fbfb158adf4e5d4a36af70f27eb49426d64
Amit Sharma <asharma11@seasiainfotech.com>
2014-10-13 17:08:08 +0530

    #pushing index controller for testing functions

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -22,7 +22,7 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         echo "<pre>";
-        print_r(KC\Repository\User::getFamousUserDetail(212));
+        print_r(KC\Repository\Offer::commongetMemberOnlyOffer('',1));
         die('END');
         $this->view->canonical = '';
         $this->view->controllerName = $this->getRequest()->getControllerName();
