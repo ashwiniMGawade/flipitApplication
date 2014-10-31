02fc80010b459e59fce8d55f36ba78962b6f7dab
Rajbir singh <rsingh6@seasiainfotech.com>
2014-10-31 18:20:04 +0530

    newsticker migration

--- library/KC/Repository/OfferNews.php
+++ library/KC/Repository/OfferNews.php
@@ -1,131 +1,115 @@
 <?php
+
 namespace KC\Repository;
-class Offer Extends \KC\Entity\Offer
-{public static function saveNewsticker($params)
-     {
-            $savenews = new OfferNews();
-            $savenews->shopId = @$params['selctedshop'];
-            $savenews->title = @BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsTitle']);
-            $savenews->url = @BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsrefUrl']);
-            $savenews->content = @BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsDescription']);
-            $savenews->linkstatus = @BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsdeepLinkStatus']);
-            $savenews->startdate = date('Y-m-d',strtotime($params['newsStartDate']));
-            $savenews->save();
 
-            //call cache
-            $key = 'shop_latestUpdates'  . intval($savenews->shopId) . '_list';
-            FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
-            return $savenews->id ;
+class OfferNews extends \KC\Entity\OfferNews
+{
+    public static function saveNewsticker($params)
+    {
+        $savenews = new \KC\Entity\OfferNews();
+        $savenews->shop = \Zend_Registry::get('emLocale')
+            ->getRepository('KC\Entity\Shop')
+            ->find($params['selctedshop']);
+        $savenews->title = @\BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsTitle']);
+        $savenews->url = @\BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsrefUrl']);
+        $savenews->content = @\BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsDescription']);
+        $savenews->linkstatus = @\BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsdeepLinkStatus']);
+        $savenews->startdate = new \DateTime($params['newsStartDate']);
+        $savenews->deleted = '0';
+        $savenews->created_at = new \DateTime('now');
+        $savenews->updated_at = new \DateTime('now');
+        \Zend_Registry::get('emLocale')->persist($savenews);
+        \Zend_Registry::get('emLocale')->flush();
+        $key = 'shop_latestUpdates'  . intval($savenews->shop->id) . '_list';
+        \FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
+        return $savenews->id;
     }
 
-  /**
-   * get newsticker to show in list
-   * @param array $params
-   * @return array $newstickerList
-   * @author blal fdf
-   * @version 1.0
-   */
-  public static function getnewstickerList($params)
-  {
-     $newstickerList = Doctrine_Query::create()
-                        ->select('n.id as id,n.shopId,n.title as title,n.startdate as startdate,s.name,n.linkstatus')
-                        ->from("OfferNews n")
-                        ->leftJoin("n.shop s")
-                        ->where("n.deleted = 0")
-                        ->orderBy("n.title ASC");
-
-        $list = DataTable_Helper::generateDataTableResponse($newstickerList,
-                $params, array("__identifier" => 'n.id', 'n.id','n.title','s.name','n.startdate','n.linkstatus'),
-                array(), array());
-
+    public static function getnewstickerList($params)
+    {
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $newstickerList =  $queryBuilder
+            ->from("KC\Entity\OfferNews", "n")
+            ->leftJoin("n.shop", "s")
+            ->where("n.deleted = 0")
+            ->orderBy("n.title", "ASC");
+        $request = \DataTable_Helper::createSearchRequest($params, array());
+        $builder  = new \NeuroSYS\DoctrineDatatables\TableBuilder(\Zend_Registry::get('emLocale'), $request);
+        $builder
+            ->setQueryBuilder($newstickerList)
+            ->add('number', 'n.id')
+            ->add('text', 'n.title')
+            ->add('text', 's.name')
+            ->add('text', 'n.startdate')
+            ->add('text', 'n.linkstatus');
+        $data = $builder->getTable()->getResultQueryBuilder()->getQuery()->getArrayResult();
+        $list = \DataTable_Helper::getResponse($data, $request);
         return $list;
-
     }
-    /**
-     * deleted newsticker by id
-     * @param integer $params
-     * @author blal
-     * @version 1.0
-     */
+
     public static function deletenewsticker($id)
     {
-        $del1 = Doctrine_Query::create()
-               ->delete()
-               ->from('OfferNews n')
-               ->where("n.id=" . $id)
-               ->execute();
-
-        //call cache
-        $key = 'shop_latestUpdates'  . intval($savenews->shopId) . '_list';
-        FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
-
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $del1 = $queryBuilder
+           ->delete("KC\Entity\OfferNews", "n")
+           ->where("n.id=" . $id)
+           ->getQuery()
+           ->execute();
+        $key = 'shop_latestUpdates'  . intval($savenews->shop->id) . '_list';
+        \FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
     }
 
-    /**
-     * details of editable newsticker
-     * @param integer $id
-     * @return array $data
-     * @author blal
-     * @version 1.0
-     */
     public static function getNewsticker($id)
     {
-        $data = Doctrine_Query::create()->select("n.*,s.name")
-                ->from('OfferNews n')
-                ->LeftJoin("n.shop s")
-                ->where("id = ?", $id)
-                ->addWhere('deleted=0')
-                ->fetchArray();
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $data = $queryBuilder->select("n,s")
+            ->from("KC\Entity\OfferNews", "n")
+            ->LeftJoin("n.shop", "s")
+            ->where("n.id = ". $id)
+            ->andWhere('n.deleted=0')
+            ->getQuery()
+            ->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $data;
     }
 
-    /**
-     * update newsticker
-     * @param integer $id
-     * @return array $data
-     * @author blal
-     * @version 1.0
-     */
     public static function updateNewsticker($params)
     {
-            $data = Doctrine_Core::getTable('OfferNews')->find( $params['id'] );
-            $data->shopId = $params['selctedshop'];
+            $data = \Zend_Registry::get('emLocale')
+                ->getRepository('KC\Entity\OfferNews')
+                ->find($params['id']);
+            $data->shop = \Zend_Registry::get('emLocale')
+                ->getRepository('KC\Entity\Shop')
+                ->find($params['selctedshop']);
             $data->offerId = null;
-            $data->title = BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsTitle']);
-            $data->url = BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsrefUrl']);
-            $data->content = BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsDescription']);
-            $data->linkstatus =  $params['newsdeepLinkStatus'];
-            $data->startdate = date('Y-m-d',strtotime($params['newsStartDate']));
-            $data->save();
-
-            //call cache
-            $key = 'shop_latestUpdates'  . intval($data->shopId) . '_list';
-            FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
-            return $savenews->id ;
+            $data->title = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsTitle']);
+            $data->url = @\BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsrefUrl']);
+            $data->content = \BackEnd_Helper_viewHelper::stripSlashesFromString($params['newsDescription']);
+            $data->linkstatus =  @$params['newsdeepLinkStatus'];
+            $data->startdate = new \DateTime($params['newsStartDate']);
+            $data->deleted = '0';
+            $data->created_at = new \DateTime('now');
+            $data->updated_at = new \DateTime('now');
+            \Zend_Registry::get('emLocale')->persist($data);
+            \Zend_Registry::get('emLocale')->flush();
+            $key = 'shop_latestUpdates'  . intval($data->shop->id) . '_list';
+            \FrontEnd_Helper_viewHelper::clearCacheByKeyOrAll($key);
+            return $data->id;
     }
 
-
-    /**
-     * getAllUrls
-     *
-     * returns the realted store page url
-     * @param integer $id  offer_news id
-     * @author Surinderpal Singh
-     * @return array array of urls
-     */
     public static function getAllUrls($id)
     {
-        $data  = Doctrine_Query::create()->select("s.permaLink,on.id")
-                ->from('OfferNews on')
-                ->leftJoin("on.shop s")
-                ->where("on.id=? " , $id)
-                ->fetchOne(null, Doctrine::HYDRATE_ARRAY);
-
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $data  = $queryBuilder->select("s,on")
+                ->from('KC\Entity\OfferNews', 'on')
+                ->leftJoin("on.shop", "s")
+                ->where("on.id= ". $id)
+                ->getQuery()
+                ->getSingleResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         $urlsArray = array();
-
         # check an offer news has related shop or not
-        if(isset($data['shop']) && $data['shop']['permaLink']) {
+        if (isset($data['shop']) && $data['shop']['permaLink']) {
             $urlsArray[] = $data['shop']['permaLink'] ;
         }
         return $urlsArray ;
     }
+}
