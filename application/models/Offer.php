05aef417cf57a05a68684951d9fdee8f6a10b58b
Rajbir singh <rsingh6@seasiainfotech.com>
2014-11-11 20:24:18 +0530

    fixed offer save

--- application/models/Offer.php
+++ application/models/Offer.php
@@ -1529,7 +1529,7 @@ class Offer extends BaseOffer
 
             }
         }
-
+        $this->extendedoffertitle = '';
         if (isset($params['extendedoffercheckbox'])) {                  // check if offer is extended
           $this->extendedOffer = BackEnd_Helper_viewHelper::stripSlashesFromString($params['extendedoffercheckbox']);
           $this->extendedTitle =BackEnd_Helper_viewHelper::stripSlashesFromString($params['extendedOfferTitle']);
