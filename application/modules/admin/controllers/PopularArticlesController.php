eeac3ef2b01acfb39cd620b404ba35f22304629d
Kuldeep Raj <kraj@seasiainfotech.com>
2014-11-20 16:33:27 +0530

    KC-4848 changes as reviewer

--- application/modules/admin/controllers/PopularArticlesController.php
+++ application/modules/admin/controllers/PopularArticlesController.php
@@ -4,14 +4,13 @@ class Admin_PopularArticlesController extends Zend_Controller_Action
 {
     public function preDispatch()
     {
-        $conn2 = BackEnd_Helper_viewHelper::addConnection(); // connection
-        $params = $this->_getAllParams();
+        $dbConnection = BackEnd_Helper_viewHelper::addConnection(); // connection
         if (!Auth_StaffAdapter::hasIdentity()) {
             $referer = new Zend_Session_Namespace('referer');
             $referer->refer = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
             $this->_redirect('/admin/auth/index');
         }
-        BackEnd_Helper_viewHelper::closeConnection($conn2);
+        BackEnd_Helper_viewHelper::closeConnection($dbConnection);
         $this->view->controllerName = $this->getRequest()->getParam('controller');
         $this->view->action = $this->getRequest()->getParam('action');
     }
