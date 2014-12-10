6cf6ae440ff4f1eb4e9e45fd216a1dfbbd5d0dc6
Kuldeep Raj <kraj@seasiainfotech.com>
2014-12-10 17:54:20 +0530

    KC-4512 remove offerDescription field from offer table

--- application/models/migrations_site/71_add_offer_description_in_offer.php
+++ application/models/migrations_site/71_add_offer_description_in_offer.php
@@ -3,17 +3,6 @@ class AddOfferDescriptionInOffer extends Doctrine_Migration_Base
 {
     public function up()
     {
-        $this->addColumn(
-            'offer',
-            'offerDescription',
-            'string',
-            1000,
-            array('notnull' => false)
-        );
-    }
-
-    public function down()
-    {
-        $this->removeColumn('offer', 'offerDescription');
+         $this->removeColumn('offer', 'offerDescription');
     }
 }
