9ddeabe60dba33faaf3a3eba496eb1a8f86b4bc1
Kuldeep Raj <kraj@seasiainfotech.com>
2014-12-20 16:24:08 +0530

    KC-488 implement confirm exit in back-end forms

---
+++ public/js/confirmExit.js
@@ -0,0 +1,24 @@
+(function ($) {
+	$.fn.confirmExit = function (message) {
+		var confirmExit = false;
+		$('input, textarea, select', this).on('change keyup', function () {
+		// Do not set the event handler if not needed
+		if (!confirmExit) {
+			confirmExit = true;
+			window.onbeforeunload = function (event) {
+			var e = event || window.event;
+			// For old IE and Firefox
+			if (e) {
+				e.returnValue = message;
+			}
+			return message;
+		}
+	}
+	});
+	this.submit(function () {
+		window.onbeforeunload = null;
+		confirmExit = false;
+	});
+	return this;
+}
+})(jQuery);
