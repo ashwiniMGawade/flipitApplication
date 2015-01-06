194989480a8c60cad678a0da9368d9b6efd8dab4
Amit Sharma <asharma11@seasiainfotech.com>
2015-01-06 18:38:40 +0530

    #made translatable social code widget

--- application/forms/SocialCode.php
+++ application/forms/SocialCode.php
@@ -10,32 +10,32 @@ class Application_Form_SocialCode extends Application_Form_Base
         $nickname = new Zend_Form_Element_Text('nickname');
         $nickname->setRequired(true);
         $nickname->setAttrib('class', 'form-control');
-        $nickname->setAttrib('placeholder', 'nickname');
+        $nickname->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_nickname'));
 
         $title = new Zend_Form_Element_Text('title');
         $title->setRequired(true);
         $title->setAttrib('class', 'form-control');
-        $title->setAttrib('placeholder', 'Offer Title');
+        $title->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_Offer Title'));
 
         $offerUrl = new Zend_Form_Element_Text('offerUrl');
         $offerUrl->setRequired(true);
         $offerUrl->setAttrib('class', 'form-control');
-        $offerUrl->setAttrib('placeholder', 'Offer URL');
+        $offerUrl->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_Offer URL'));
 
         $code = new Zend_Form_Element_Text('code');
         $code->setRequired(true);
         $code->setAttrib('class', 'form-control');
-        $code->setAttrib('placeholder', 'Coupon code');
+        $code->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_Coupon code'));
 
         $expireDate = new Zend_Form_Element_Text('expireDate');
         $expireDate->setRequired(true);
         $expireDate->setAttrib('class', 'form-control');
-        $expireDate->setAttrib('placeholder', 'Expire Date');
+        $expireDate->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_Expire Date'));
 
         $offerDetails = new Zend_Form_Element_Textarea('offerDetails');
         $offerDetails->setRequired(true);
         $offerDetails->setAttrib('class', 'form-control');
-        $offerDetails->setAttrib('placeholder', 'Offer Details');
+        $offerDetails->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_Offer Details'));
 
         $shopIdHiddenField =  new Zend_Form_Element_Hidden('shopId');
         $shopPermalinkHiddenField =  new Zend_Form_Element_Hidden('shopPermalink');
