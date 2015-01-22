7069113f2aa502ebf855376986b8e0906e415409
Kuldeep Raj <kraj@seasiainfotech.com>
2015-01-22 13:46:58 +0530

    KC-5257 migrate popular special page model

--- library/KC/Repository/SpecialPagesOffers.php
+++ library/KC/Repository/SpecialPagesOffers.php
@@ -6,28 +6,27 @@ class SpecialPagesOffers extends \KC\Entity\SpecialPagesOffers
 {
     public static function getSpecialPageOffersByPageIdForFrontEnd($pageId)
     {
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
         $currentDate = date("Y-m-d H:i");
-        $specialPageOffers = Doctrine_Query::create()
+        $query = $queryBuilder
         ->select(
             'op.pageId,op.offerId,o.couponCodeType,o.totalViewcount as clicks,o.title,o.refURL,o.refOfferUrl,
             o.discountType,o.startDate,o.endDate,o.authorId,o.authorName,o.Visability,o.couponCode,o.exclusiveCode,
             o.editorPicks,o.discount,o.discountvalueType,o.startdate,o.extendedOffer,o.extendedUrl,
             o.updated_at as lastUpdate,s.name,s.refUrl,
-            s.actualUrl,s.permaLink as permalink,s.views,l.*,fv.id,fv.visitorId,fv.shopId,vot.id,vot.vote, ologo.path,
-            ologo.name,terms.content'
+            s.actualUrl,s.permaLink as permalink,s.views,l.*,fv.id,fv.visitorId,fv.shopId,vot.id,vot.vote,terms.content'
         )
-        ->from('SpecialPagesOffers op')
-        ->leftJoin('op.offers o')
-        ->leftJoin('o.logo ologo')
-        ->leftJoin('o.termandcondition terms')
+        ->from('KC\Entity\SpecialPagesOffers', 'op')
+        ->leftJoin('op.offers', 'o')
+        ->leftJoin('o.termandcondition', 'terms')
         ->andWhere(
-            "(couponCodeType = 'UN' AND (SELECT count(id) FROM CouponCode cc WHERE cc.offerid = o.id and status=1)  > 0)
+            "(couponCodeType = 'UN' AND (SELECT count(id) FROM KC\Entity\CouponCode cc WHERE cc.offerid = o.id and status=1)  > 0)
             or couponCodeType = 'GN'"
         )
-        ->leftJoin('o.shop s')
-        ->leftJoin('o.vote vot')
-        ->leftJoin('s.logo l')
-        ->leftJoin('s.favoriteshops fv')
+        ->leftJoin('o.shop', 's')
+        ->leftJoin('o.vote', 'vot')
+        ->leftJoin('s.logo', 'l')
+        ->leftJoin('s.favoriteshops', 'fv')
         ->where('op.pageId = '.$pageId)
         ->andWhere('o.enddate > "'.$currentDate.'"')
         ->andWhere('o.startdate <= "'.$currentDate.'"')
@@ -35,8 +34,8 @@ class SpecialPagesOffers extends \KC\Entity\SpecialPagesOffers
         ->andWhere('s.deleted = 0')
         ->andWhere('s.status = 1')
         ->andWhere('o.Visability!="MEM"')
-        ->orderBy('op.position')
-        ->fetchArray();
+        ->orderBy('op.position');
+        $specialPageOffers = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return self::removeDuplicateOffers($specialPageOffers);
     }
 
@@ -54,49 +53,54 @@ class SpecialPagesOffers extends \KC\Entity\SpecialPagesOffers
 
     public static function getSpecialPageOfferById($pageId)
     {
-        $specialPageOffers = Doctrine_Query::create()
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder
             ->select('p.id, p.pageId, p.offerId, o.title, p.position')
-            ->from('SpecialPagesOffers p')
-            ->leftJoin('p.offers o')
-            ->where('p.pageId ='.$pageId)
-            ->orderBy('p.position')
-            ->fetchArray();
+            ->from('KC\Entity\SpecialPagesOffers', 'p')
+            ->leftJoin('p.offers', 'o')
+            ->where('p.pages ='.$pageId)
+            ->orderBy('p.position');
+        $specialPageOffers = $query->getQuery()->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         return $specialPageOffers;
     }
 
     public static function addOfferInList($offerId, $pageId)
     {
-        $offer = Doctrine_query::create()
-            ->from('Offer')
-            ->where('id=' . $offerId)->limit(1)
-            ->fetchArray();
+        $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+        $query = $queryBuilder
+            ->select('o')
+            ->from('KC\Entity\Offer', 'o')
+            ->where('id=' . $offerId)
+            ->setMaxResults(1);
+        $offer = $query->getQuery()->getSingleResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         $result = '0';
         if (sizeof($offer) > 0) {
-            $specialPageOffer = Doctrine_query::create()
-                ->from('SpecialPagesOffers')
-                ->where('offerId=' . $offerId)
-                ->andWhere('pageId=' .$pageId)
-                ->limit(1)
-                ->fetchArray();
+                $query = $queryBuilder
+                    ->select('sl')
+                    ->from('KC\Entity\SpecialPagesOffers', 'sl')
+                    ->where('sl.offers=' . $offerId)
+                    ->andWhere('sl.pages=' .$pageId)
+                    ->setMaxResults(1);
+                $specialPageOffer = $query->getQuery()->getSingleResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
             if (!empty($specialPageOffer)) {
                 $result = '2';
             } else {
                 $result = '1';
-                $maxPosition = Doctrine_Query::create()
+                $query = $queryBuilder
                     ->select('p.position')
-                    ->from('SpecialPagesOffers p')
-                    ->where('pageId=' .$pageId)
-                    ->orderBy('p.position DESC')
-                    ->limit(1)
-                    ->fetchArray();
+                    ->from('KC\Entity\SpecialPagesOffers', 'p')
+                    ->where('pages=' .$pageId)
+                    ->orderBy('p.position', 'DESC')
+                    ->setMaxResults(1);
+                $maxPosition = $query->getQuery()->getSingleResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
                 if (!empty($maxPosition)) {
                     $newPosition = $maxPosition[0]['position'];
                 } else {
                     $newPosition =  0 ;
                 }
-                $specialPageOffer = new SpecialPagesOffers();
-                $specialPageOffer->offerId = $offerId;
-                $specialPageOffer->pageId = $pageId;
+                $specialPageOffer = new KC\Repository\SpecialPagesOffers();
+                $specialPageOffer->offers = $offerId;
+                $specialPageOffer->pages = $pageId;
                 $specialPageOffer->position = (intval($newPosition) + 1);
                 $specialPageOffer->save();
                 $result  = array(
@@ -116,29 +120,36 @@ class SpecialPagesOffers extends \KC\Entity\SpecialPagesOffers
     public static function deleteCode($id, $position, $pageId)
     {
         if ($id) {
-            $deleteCode = Doctrine_Query::create()
-            ->delete('SpecialPagesOffers')
-            ->where('id=' . $id)
-            ->execute();
-            $updatePosition = Doctrine_Query::create()
-                ->update('SpecialPagesOffers p')
-                ->set('p.position', 'p.position -1')
-                ->where('p.position >' . $position)
-                ->andWhere('p.pageId='. $pageId)
-                ->execute();
-            $newOffersList = Doctrine_Query::create()
-                ->select('p.*')
-                ->from('SpecialPagesOffers p')
-                ->where('p.pageId='. $pageId)
-                ->orderBy('p.position ASC')
-                ->fetchArray();
+            $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+            $query = $queryBuilder
+                ->delete('KC\Entity\SpecialPagesOffers', 'spl')
+                ->where('spl.id ='.$id)
+                ->getQuery();
+            $query->execute();
+
+            $query = $queryBuilder
+                ->update('KC\Entity\SpecialPagesOffers', 'p')
+                ->set('p.position', $queryBuilder->expr()->literal('p.position -1'))
+                ->where('p.position', $queryBuilder->expr()->gt($position))
+                ->andWhere('p.pages='. $pageId)
+                ->getQuery();
+            $query->execute();
+             
+            $query = $queryBuilder
+                ->select('p')
+                ->from('KC\Entity\SpecialPagesOffers', 'p')
+                ->where('p.pages='. $pageId)
+                ->orderBy('p.position', 'ASC');
+            $newOffersList = $query->getQuery()->getSingleResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
+
             $newPosition = 1;
             foreach ($newOffersList as $newOffer) {
-                $updateWithNewPosition = Doctrine_Query::create()
-                    ->update('SpecialPagesOffers p')
-                    ->set('position', $newPosition)
-                    ->where('p.id = ?', $newOffer['id']);
-                $updateWithNewPosition->execute();
+                $query = $queryBuilder
+                    ->update('KC\Entity\SpecialPagesOffers', 'p')
+                    ->set('p.position', $newPosition)
+                    ->where('p.id = ', $newOffer['id'])
+                    ->getQuery();
+                $query->execute();
                 $newPosition++;
             }
             self::clearCacheOfSpecialPagesOffers($pageId);
@@ -150,16 +161,18 @@ class SpecialPagesOffers extends \KC\Entity\SpecialPagesOffers
     public static function savePosition($offerIds, $pageId)
     {
         if (!empty($offerIds)) {
-            $deleteCode = Doctrine_Query::create()
-                ->delete('SpecialPagesOffers p')
-                ->where('p.pageId=' . $pageId)
-                ->execute();
+            $queryBuilder = \Zend_Registry::get('emLocale')->createQueryBuilder();
+            $query = $queryBuilder
+                ->delete('KC\Entity\SpecialPagesOffers', 'spl')
+                ->where('spl.pages ='.$pageId)
+                ->getQuery();
+            $query->execute();
             $offerIds = explode(',', $offerIds);
             $i = 1;
             foreach ($offerIds as $offerId) {
-                $specialPageOffer = new SpecialPagesOffers();
-                $specialPageOffer->offerId = $offerId;
-                $specialPageOffer->pageId = $pageId;
+                $specialPageOffer = new KC\Entity\SpecialPagesOffers();
+                $specialPageOffer->offers = $offerId;
+                $specialPageOffer->pages = $pageId;
                 $specialPageOffer->position = $i;
                 $specialPageOffer->save();
                 $i++;
