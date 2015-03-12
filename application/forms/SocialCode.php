dd2f1a3b94df3608e2d23b6479643b909d36b42b
Kuldeep Raj <kraj@seasiainfotech.com>
2015-03-12 14:18:21 +0530

    translation issue for date place holder

--- application/forms/SocialCode.php
+++ application/forms/SocialCode.php
@@ -31,7 +31,7 @@ class Application_Form_SocialCode extends Application_Form_Base
         
         $expireDate = new Zend_Form_Element_Text('expireDate');
         $expireDate->setAttrib('class', 'form-control');
-        $expireDate->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_expirydate(dd-mm-YYYY optional)'));
+        $expireDate->setAttrib('placeholder', FrontEnd_Helper_viewHelper::__form('form_expirydate'));
         $expireDate->addValidator('regex', false, array('/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(|-|)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(|-|)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(|-|)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/'));
         $expireDate->addValidator(new Application_Form_DateGreaterThanToday());
         $expireDate->addFilter('StringTrim');
