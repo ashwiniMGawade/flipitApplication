ea0d4600e934dab4c1850e14731a15004481de41
Kuldeep Raj <kraj@seasiainfotech.com>
2015-04-17 14:26:18 +0530

    KC-4954 changes as per code reviewer

--- application/controllers/OutController.php
+++ application/controllers/OutController.php
@@ -6,7 +6,6 @@ class OutController extends Zend_Controller_Action
         $offerId = $this->getRequest()->getParam('id');
         FrontEnd_Helper_viewHelper::viewCounter('offer', 'onclick', $offerId);
         $conversionId = Conversions::addConversion($offerId, 'offer');
-        ViewCount::updateCacheValueForOfferViewCount($offerId);
         $clickout = new FrontEnd_Helper_ClickoutFunctions($offerId, null);
         $redirectUrl = $clickout->getCloakLink('offer', $conversionId);
         $this->_helper->redirector->setCode(301);
