3b7f9abcc45405d934e49666aa169d9370800e21
Kuldeep Raj <kraj@seasiainfotech.com>
2015-04-27 10:08:13 +0530

    KC-6063 get location and display in text field

--- application/modules/admin/controllers/WidgetlocationController.php
+++ application/modules/admin/controllers/WidgetlocationController.php
@@ -3,7 +3,7 @@ class Admin_WidgetlocationController extends Zend_Controller_Action
 {
     public function preDispatch()
     {
-        $dbConnection = \BackEnd_Helper_viewHelper::addConnection();
+        $dbConnection = BackEnd_Helper_viewHelper::addConnection();
         if (!Auth_StaffAdapter::hasIdentity()) {
             $referer = new Zend_Session_Namespace('referer');
             $referer->refer = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
@@ -17,12 +17,12 @@ class Admin_WidgetlocationController extends Zend_Controller_Action
     public function saveOrUpdateWidgetLocationAction()
     {
         $parameters = $this->getRequest()->getParams();
-        $saveWidgetLocationStatus = false;
+        $savedOrUpdatedWidgetLocationStatus = false;
         if (!empty($parameters['widgetPostion']) && !empty($parameters['widgetLocation'])) {
             KC\Repository\WidgetLocation::saveOrUpdateWidgetLocation($parameters);
-            $saveWidgetLocationStatus = true;
+            $savedOrUpdatedWidgetLocationStatus = true;
         }
-        echo Zend_Json::encode($saveWidgetLocationStatus);
+        echo Zend_Json::encode($savedOrUpdatedWidgetLocationStatus);
         die();
     }
 }
