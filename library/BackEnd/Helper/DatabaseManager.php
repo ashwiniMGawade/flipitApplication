4424292744045b4b801b5bff8fd782114c631e8a
Kim Pellikaan <kim@web-flight.nl>
2015-04-29 22:40:50 +0200

    Setup Doctrine connection without the Setup class. This was causing memcache to be used.

--- library/BackEnd/Helper/DatabaseManager.php
+++ library/BackEnd/Helper/DatabaseManager.php
@@ -1,9 +1,11 @@
 <?php
-use Doctrine\ORM\Tools\Setup;
+// use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
+
 class BackEnd_Helper_DatabaseManager
 {
     /**
@@ -12,24 +14,39 @@ class BackEnd_Helper_DatabaseManager
     */
     public static function addConnection($key = 'be')
     {
-        # read dsn from confiog file an dcreta new contien connection
+        # read dsn from config file an create new connection.
         $bootstrap = \Zend_Controller_Front::getInstance()->getParam('bootstrap');
         $options = $bootstrap->getOptions();
         $key = strtolower($key);
         $connName = "dynamic_conn_" . $key;
         $dsn = $options['doctrine'][$key]['dsn'];
+
+        # setup memcached
+        $memcacheHostParams = $options['resources']['frontController']['params']['memcache'];
+        $splitMemcacheValues = explode(':', $memcacheHostParams);
+        $memcachePort = isset($splitMemcacheValues[1]) ? $splitMemcacheValues[1] : '';
+        $memcacheHost = isset($splitMemcacheValues[0]) ? $splitMemcacheValues[0] : '';
+        $memcache = new Memcached();
+        $memcache->addServer($memcacheHost, $memcachePort);
+        $cache = new \Doctrine\Common\Cache\MemcachedCache;
+        $cache->setMemcached($memcache);
+        $annotationReader = new Doctrine\Common\Annotations\AnnotationReader;
+        $cachedAnnotationReader = new Doctrine\Common\Annotations\CachedReader(
+            $annotationReader,
+            $cache
+        );
+
         $paths = array(APPLICATION_PATH . '/../library/KC/Entity');
-        $isDevMode = false;
-        $config = Setup::createConfiguration($isDevMode);
-        $driver = new AnnotationDriver(new AnnotationReader(), $paths);
-        // registering noop annotation autoloader - allow all annotations by default
+        $driver = new AnnotationDriver($cachedAnnotationReader, $paths);
         AnnotationRegistry::registerLoader('class_exists');
+
+        $config = new Configuration();
         $config->setMetadataDriverImpl($driver);
-        // set the proxy dir and set some options
         $config->setProxyDir(APPLICATION_PATH . '/../library/KC/Entity/Proxy');
         $config->setAutoGenerateProxyClasses(true);
         $config->setProxyNamespace('KC\Entity\Proxy');
-        # create a nrew connectoion based on select dsn
+        
+        # create a new connection based on select dsn
         $connectionParamsLocale = BootstrapDoctrineConnectionFunctions::getDatabaseCredentials($dsn);
         $emLocale = EntityManager::create($connectionParamsLocale, $config);
         \Zend_Registry::set('emLocale', $emLocale);
