58291164a85a6664b38218a479a6eaf3903859be
Kuldeep Raj <kraj@seasiainfotech.com>
2015-04-30 16:38:51 +0530

    KC-4352 add offer as specific time

--- library/FrontEnd/Helper/viewHelper.php
+++ library/FrontEnd/Helper/viewHelper.php
@@ -1138,7 +1138,7 @@ EOD;
         $localeSettings = KC\Repository\LocaleSettings::getLocaleSettings();
         $localeTimezone = !empty($localeSettings[0]['timezone']) ? $localeSettings[0]['timezone'] : 'Europe/Amsterdam';
         $refreshTime = new DateTime($offerTime, new DateTimeZone($localeTimezone));
-        $refreshTime->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+        $refreshTime->setTimezone(new DateTimeZone('GMT'));
         return $refreshTime->format('Y-m-d H:i:s');
     }
 
@@ -1147,7 +1147,7 @@ EOD;
         $localeSettings = KC\Repository\LocaleSettings::getLocaleSettings();
         $localeTimezone = !empty($localeSettings[0]['timezone']) ? $localeSettings[0]['timezone'] : 'Europe/Amsterdam';
         $refreshTime = new DateTime('now', new DateTimeZone($localeTimezone));
-        $refreshTime->setTimezone(new DateTimeZone('Europe/Amsterdam'));
+        $refreshTime->setTimezone(new DateTimeZone('GMT'));
         return $refreshTime->format('Y-m-d H:i:s');
     }
 }
