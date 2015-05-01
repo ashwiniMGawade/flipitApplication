2c95930880f803f3b58739b0d349f8e928d6b18b
Kim Pellikaan <kim@web-flight.nl>
2015-05-01 16:25:43 +0200

    Updated CLI-config so that production cache is set to memcache so that can be flushed on deploy.

--- library/bin/cli-config.php
+++ library/bin/cli-config.php
@@ -1,16 +1,47 @@
 <?php
+
+/*
+Needs a massive refactor!! Needs to use BootstrapDoctrineConnectionFunctions to make it DRY. 
+Also the ENV is not working as that is not set in the commandline.
+ */
+
+use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Configuration;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\ORM\Tools\Setup;
+
+// Define default application environment
+defined('APPLICATION_ENV') ||
+    define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV'): 'production'));
+
+// Get Memcached settings from application.ini
+require_once 'Zend/Config/Ini.php';
+$config = new Zend_Config_Ini('../application/configs/application.ini', APPLICATION_ENV);
+$memcacheEndpoint = $config->resources->frontController->params->memcache;
+$splitMemcacheValues = explode(':', $memcacheEndpoint);
+$memcachePort = isset($splitMemcacheValues[1]) ? $splitMemcacheValues[1] : '';
+$memcacheHost = isset($splitMemcacheValues[0]) ? $splitMemcacheValues[0] : '';
+$memcache = new Memcached();
+$memcache->addServer($memcacheHost, $memcachePort);
+$cache = new \Doctrine\Common\Cache\MemcachedCache;
+$cache->setMemcached($memcache);
+$isDevMode = false;
+$proxyPath = null;
+if (APPLICATION_ENV == 'development') {
+    $cache = null;
+    $isDevMode = true;
+    $proxyPath = APPLICATION_PATH . '/../library/KC/Entity/Proxy';
+}
+
+$config = Setup::createConfiguration($isDevMode, $proxyPath, $cache);
+
 $paths = array('../library/KC/Entity');
-$config = Setup::createConfiguration(false);
 $driver = new AnnotationDriver(new AnnotationReader(), $paths);
 AnnotationRegistry::registerLoader('class_exists');
 $config->setProxyNamespace('Proxy');
 $config->setMetadataDriverImpl($driver);
+
 $dsn = array(
     'host' => 'localhost',
     'driver' => 'pdo_mysql',
