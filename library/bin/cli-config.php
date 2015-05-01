0c76d582e3f91bcb9c6f01b54e55284e5fb1c6b2
Kuldeep Raj <kraj@seasiainfotech.com>
2015-05-01 13:55:26 +0530

    Configure cli for orm

---
+++ library/bin/cli-config.php
@@ -0,0 +1,24 @@
+<?php
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\Tools\Setup;
+$paths = array('../library/KC/Entity');
+$config = Setup::createConfiguration(false);
+$driver = new AnnotationDriver(new AnnotationReader(), $paths);
+AnnotationRegistry::registerLoader('class_exists');
+$config->setMetadataDriverImpl($driver);
+$drivers = array(
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+    'user' => 'root',
+    'password' =>
+    'root', 'dbname' => 'flipit_in'
+);
+$em = EntityManager::create($drivers, $config);
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+));
