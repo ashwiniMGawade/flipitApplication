838ae012ab0c9fbdc36a43c385ce7657b2d4acf7
Rajbir singh <rsingh6@seasiainfotech.com>
2015-05-13 13:15:17 +0530

    working on top 20 page header changes

--- library/FrontEnd/Helper/PageHeaderPartialFunctions.php
+++ library/FrontEnd/Helper/PageHeaderPartialFunctions.php
@@ -16,4 +16,22 @@ class FrontEnd_Helper_PageHeaderPartialFunctions
         $imageName = isset($headerImage['name']) ? $headerImage['name'] : '';
         return PUBLIC_PATH_CDN. $imagePath . $imageName;
     }
+
+    public function getTop20Header($headerText, $headerSubText, $headerImage)
+    {
+        $headertitle = '<h1>' . $headerText . '</h1>';
+        $headerSubTitle = '<h2>' . $headerSubText . '</h2>';
+        $header = '<div class="header-block top-header-block"> 
+            <div class="icon">
+                <a href=""><img width="50" height="50" alt="' . $headerText . '" title="' . $headerText . '"  
+                    src="' .  $headerImage . '" class="radiusImg"></a>
+            </div>
+            <div class="box">
+                <div class="holder">
+                    '.$headertitle.$headerSubTitle.'
+                </div>
+            </div>
+        </div>';
+        return $header;
+    }
 }
