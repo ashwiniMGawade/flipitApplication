0e2e29e30430f978415a729599f01a720cdd31e6
Rajbir singh <rsingh6@seasiainfotech.com>
2015-05-14 18:13:55 +0530

    fixed plus page

--- library/FrontEnd/Helper/MoneySavingGuidesPartialFunctions.php
+++ library/FrontEnd/Helper/MoneySavingGuidesPartialFunctions.php
@@ -5,66 +5,68 @@ class FrontEnd_Helper_MoneySavingGuidesPartialFunctions
     {
         $relatedArticles = '<div class="row articles-box">';
         foreach ($categoryWiseAllArticles as $article) {
-            $profileLink = HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::__link("link_redactie")."/"
-                    . $article['authorDetails']['slug'];
-            $articleUpdatedAtDate = $article['articles']['publishdate'];
-            $articleUpdatedAtDate = new Zend_Date($articleUpdatedAtDate->date);
-            $articleUpdatedAtDate = $articleUpdatedAtDate->get(Zend_Date::DATE_LONG);
-            $authorName = FrontEnd_Helper_AuthorPartialFunctions::getAuthorName(
-                $article['authorDetails']['firstName'],
-                $article['authorDetails']['lastName']
-            );
-            $articleImage = !empty($article['articles']['thumbnail']) ?
-                PUBLIC_PATH_CDN.$article['articles']['thumbnail']['path'].$article['articles']['thumbnail']['name'] : '';
+            if (!empty($article['authorDetails'])) {
+                $profileLink = HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::__link("link_redactie")."/"
+                        . $article['authorDetails']['slug'];
+                $articleUpdatedAtDate = $article['articles']['publishdate'];
+                $articleUpdatedAtDate = new Zend_Date($articleUpdatedAtDate->date);
+                $articleUpdatedAtDate = $articleUpdatedAtDate->get(Zend_Date::DATE_LONG);
+                $authorName = FrontEnd_Helper_AuthorPartialFunctions::getAuthorName(
+                    $article['authorDetails']['firstName'],
+                    $article['authorDetails']['lastName']
+                );
+                $articleImage = !empty($article['articles']['thumbnail']) ?
+                    PUBLIC_PATH_CDN.$article['articles']['thumbnail']['path'].$article['articles']['thumbnail']['name'] : '';
 
-            if (isset($article['articles']['plusTitle']) && $article['articles']['plusTitle'] != '') {
-                $articleTitle = mb_strlen($article['articles']['plusTitle']) > 50
-                    ? mb_substr($article['articles']['plusTitle'], 0, 50).'..'
-                    : $article['articles']['plusTitle'];
-                $altTitle = $article['articles']['plusTitle'];
-            } else {
-                $articleTitle = mb_strlen($article['articles']['title']) > 50
-                    ? mb_substr($article['articles']['title'], 0, 50).'..'
-                    : $article['articles']['title'];
-                $altTitle = $article['articles']['title'];
-            }
-            
-            $articleBy = $authorName != '' ? FrontEnd_Helper_viewHelper::__translate('By') : '';
-            $categoryTitleBackgroundColor = !empty($article['articles']['category'][0]['categorytitlecolor'])
-                                                ? $article['articles']['category'][0]['categorytitlecolor']
-                                                : 'e69342';
-            $relatedArticles .=
-                    '<article class="article col-md-3 col-sm-4 col-xs-6 ">
-                        <div class="image">
-                            <span class="category" style = "background :#'.$categoryTitleBackgroundColor.';">
-                                '.$article['artcileCategoryType'].'
-                            </span>
-                            <a href= "'.HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::getPagePermalink().'/'
-                                .$article['articles']['permalink'].'">
-                                <img class="lazy" data-original="'.$articleImage.'"
-                                width="270" height="192" alt="'.$altTitle.'" title="'.$altTitle.'">
-                                <noscript>
-                                    <img src="'.$articleImage.'" width="270" height="192" alt="'.$altTitle.'" title="'.$altTitle.'">
-                                </noscript>
-                            </a>    
-                        </div>
-                        <div class="holder">
-                            <div class="box">
-                                <h2>
-                                    <a href="'.HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::getPagePermalink().'/'
-                                        .$article['articles']['permalink'].'">
-                                       '.$articleTitle.'
-                                    </a>
-                                </h2>
-                            </div>
-                            <div class="meta">
-                                <span class="author">'.$articleBy.'
-                                    <a href="'.$profileLink.'">'.$authorName.'</a>
+                if (isset($article['articles']['plusTitle']) && $article['articles']['plusTitle'] != '') {
+                    $articleTitle = mb_strlen($article['articles']['plusTitle']) > 50
+                        ? mb_substr($article['articles']['plusTitle'], 0, 50).'..'
+                        : $article['articles']['plusTitle'];
+                    $altTitle = $article['articles']['plusTitle'];
+                } else {
+                    $articleTitle = mb_strlen($article['articles']['title']) > 50
+                        ? mb_substr($article['articles']['title'], 0, 50).'..'
+                        : $article['articles']['title'];
+                    $altTitle = $article['articles']['title'];
+                }
+                
+                $articleBy = $authorName != '' ? FrontEnd_Helper_viewHelper::__translate('By') : '';
+                $categoryTitleBackgroundColor = !empty($article['articles']['category'][0]['categorytitlecolor'])
+                                                    ? $article['articles']['category'][0]['categorytitlecolor']
+                                                    : 'e69342';
+                $relatedArticles .=
+                        '<article class="article col-md-3 col-sm-4 col-xs-6 ">
+                            <div class="image">
+                                <span class="category" style = "background :#'.$categoryTitleBackgroundColor.';">
+                                    '.$article['artcileCategoryType'].'
                                 </span>
-                                <em class="date">'.$articleUpdatedAtDate.'</em>
+                                <a href= "'.HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::getPagePermalink().'/'
+                                    .$article['articles']['permalink'].'">
+                                    <img class="lazy" data-original="'.$articleImage.'"
+                                    width="270" height="192" alt="'.$altTitle.'" title="'.$altTitle.'">
+                                    <noscript>
+                                        <img src="'.$articleImage.'" width="270" height="192" alt="'.$altTitle.'" title="'.$altTitle.'">
+                                    </noscript>
+                                </a>    
                             </div>
-                        </div>
-                    </article>';
+                            <div class="holder">
+                                <div class="box">
+                                    <h2>
+                                        <a href="'.HTTP_PATH_LOCALE.FrontEnd_Helper_viewHelper::getPagePermalink().'/'
+                                            .$article['articles']['permalink'].'">
+                                           '.$articleTitle.'
+                                        </a>
+                                    </h2>
+                                </div>
+                                <div class="meta">
+                                    <span class="author">'.$articleBy.'
+                                        <a href="'.$profileLink.'">'.$authorName.'</a>
+                                    </span>
+                                    <em class="date">'.$articleUpdatedAtDate.'</em>
+                                </div>
+                            </div>
+                        </article>';
+            }
         }
         echo $relatedArticles .'</div>';
     }
