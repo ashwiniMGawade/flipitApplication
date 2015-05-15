355583e1e81bde43f5a1cebc06a17af794e0fa2f
Daniel Bakker <info@danielbakker.nl>
2015-05-15 15:26:47 +0200

    Revert "how to slug problem"
    This reverts commit 72acd8337c160d3afa79ae238ac3bed90386818e.

--- public/js/back_end/edit-shop.js
+++ public/js/back_end/edit-shop.js
@@ -852,6 +852,10 @@ function validateFormAddNewShop(){
                         regex : /^([1-9]|10)$/
                     },
                     howToPageSlug :{
+                        required : function(el)
+                        {
+                            return $('#howTouseStatus').val() == 1;
+                        },
                         slugRegex  : function(element) {
                             return $('#howTouseStatus').val() == 1;
                         }
