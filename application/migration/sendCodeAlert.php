03695a00f179eba4ad2373481e7292c35f8d3a19
Rajbir singh <rsingh6@seasiainfotech.com>
2015-05-19 19:11:03 +0530

    fixed code alert delay

--- application/migration/sendCodeAlert.php
+++ application/migration/sendCodeAlert.php
@@ -135,7 +135,7 @@ class SendCodeAlert
         if (!empty($codeAlertOffers)) {
             foreach ($codeAlertOffers as $codeAlertOffer) {
                 $this->shopId = $codeAlertOffer['shop']['id'];
-                $currentDate = date('Y-m-d H:i:s', strtotime('+1 hour'));
+                $currentDate = date('Y-m-d H:i:s');
                 $startDateTimestamp = strtotime($codeAlertOffer['startDate']) + 3600;
                 $startDate = date('Y-m-d H:i:s', $startDateTimestamp);
 
@@ -143,7 +143,7 @@ class SendCodeAlert
                     CodeAlertQueue::moveCodeAlertToTrash($codeAlertOffer['id']);
                 }
 
-                if (($startDate <= $currentDate && $codeAlertOffer['endDate'] >= date('Y-m-d H:i:s')) && $codeAlertOffer['offline'] == 0) {
+                if (($startDate <= $currentDate && $codeAlertOffer['endDate'] >= $currentDate) && $codeAlertOffer['offline'] == 0) {
                     $this->setPhpExecutionLimit();
                     $topVouchercodes = FrontEnd_Helper_viewHelper::getShopCouponCode(
                         'similarStoresAndSimilarCategoriesOffers',
