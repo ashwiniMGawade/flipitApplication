c2a0bb64f9daebc88b76b0d376296b1a6a67e779
Kim Pellikaan <kim@web-flight.nl>
2015-05-20 16:07:38 +0200

    Fixed a wrong space

--- library/BackEnd/Helper/CacheManager.php
+++ library/BackEnd/Helper/CacheManager.php
@@ -2,7 +2,7 @@
 class BackEnd_Helper_CacheManager
 {
     public static function clearResultCache($cacheId) {
-        $entityManagerLocale  = \Zend_Registry::get('emLocale')
+        $entityManagerLocale = \Zend_Registry::get('emLocale')
             ->getConfiguration()
             ->getResultCacheImpl()
             ->delete($cacheId);
