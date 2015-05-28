d5c32a8ba34d1e072fca85eb23da058c00a5fb17
Kuldeep Raj <kraj@seasiainfotech.com>
2015-05-28 17:53:05 +0530

    Create first unit test

--- application/services/Offer/OfferListing.php
+++ application/services/Offer/OfferListing.php
@@ -1,10 +1,6 @@
 <?php
 class Application_Service_Offer_OfferListing
 {
-    public function abc()
-    {
-        echo "WELCOME";
-        die;
-    }
+
 }
 new Application_Service_Offer_OfferListing();
