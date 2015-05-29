b5303f2821add48d693e5a4b8ce458dd7b82a98d
Rajbir singh <rsingh6@seasiainfotech.com>
2015-05-29 10:56:19 +0530

    added dynamic session timeout in login controller

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -129,7 +129,7 @@ class LoginController extends Zend_Controller_Action
     public function logoutAction()
     {
         \Auth_VisitorAdapter::clearIdentity();
-        setcookie('kc_unique_user_id', "", time() - (86400 * 3), '/');
+        setcookie('kc_unique_user_id', "", time() - (Session_Helper_Session::getSessionTimeout()), '/');
         # set reponse header X-Nocache used for varnish
         $this->getResponse()->setHeader('X-Nocache', 'no-cache');
         \Zend_Session::namespaceUnset('favouriteShopId');
