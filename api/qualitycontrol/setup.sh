1ae08d4aa511da0d4d7fd32de54fa4e8f9ac606b
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-06-29 19:01:26 +0530

    KC-6601 #comment Adding the PHPMD ruleset

--- api/qualitycontrol/setup.sh
+++ api/qualitycontrol/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp qualitycontrol/pre-commit ../../.git/modules/flipit_application/hooks
-cp qualitycontrol/commit-msg ../../.git/modules/flipit_application/hooks
+#cp qualitycontrol/commit-msg ../../.git/modules/flipit_application/hooks
 chmod +x ../../.git/modules/flipit_application/hooks/pre-commit
-chmod +x ../../.git/modules/flipit_application/hooks/commit-msg
+#chmod +x ../../.git/modules/flipit_application/hooks/commit-msg
