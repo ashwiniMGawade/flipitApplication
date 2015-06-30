518cdc585119d18f1663af6a12a2327a5a7afc5c
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-06-30 20:12:44 +0530

    KC-6606 #comment Fixing Database path

--- core/Persistence/Database/Service/DatabaseConnection.php
+++ core/Persistence/Database/Service/DatabaseConnection.php
@@ -4,7 +4,7 @@ class DatabaseConnection
 {
     public static function getDsn($key = 'en')
     {
-        $ini_array = parse_ini_file("..\..\..\..\web\application\configs\application.ini");
+        $ini_array = parse_ini_file(__DIR__."/../../../../web/application/configs/application.ini");
         $dsnKeyValue = 'doctrine.'.$key.'.dsn';
         if ($key == 'imbull') {
             $dsnKeyValue = 'doctrine.'.$key;
