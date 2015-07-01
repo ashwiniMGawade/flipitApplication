968ad391d513ab600ee32dfec577c658a73701b4
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-01 11:45:44 +0530

    KC-6601 #comment REmoving quality control from the API directory

--- api/qualitycontrol/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp qualitycontrol/pre-commit qualitycontrol/commit-msg ../../.git/modules/flipit_application/hooks
-chmod +x ../../.git/modules/flipit_application/hooks/pre-commit ../../.git/modules/flipit_application/hooks/commit-msg
