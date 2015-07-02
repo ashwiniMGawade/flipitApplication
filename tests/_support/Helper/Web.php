ac6feace0ec8222b54969f0e58060bee2b673399
Kim Pellikaan <kim@web-flight.nl>
2015-07-02 23:40:13 +0200

    KC-1 #comment After removing the extension I brought the DB reset to the website helper.

--- tests/_support/Helper/Web.php
+++ tests/_support/Helper/Web.php
@@ -1,9 +1,40 @@
 <?php
 namespace Helper;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class Web extends \Codeception\Module
 {
+    public function _before(\Codeception\TestCase $test)
+    {
+        $databaseHelper = new \Tests\WebDatabaseHelper;
 
+        $application = new \Zend_Application(APPLICATION_ENV, __DIR__."/../../../web/application/configs/application.ini");
+        $applicationConfig = $application->getOption('doctrine');
+
+        $databases[] = $databaseHelper->getDatabaseCredentials($applicationConfig['en']['dsn']);
+        $databases[] = $databaseHelper->getDatabaseCredentials($applicationConfig['imbull']);
+
+        foreach ($databases as $database) {
+            
+            $sqlDumpPath = 'tests/_data/' . $database['dbname'] . '.sql';
+            
+            // Drop and create database
+            $databaseHelper
+                ->connect('mysql:host=' . $database['host'] . ';', $database['user'], $database['password'])
+                ->restart($database['dbname']);
+
+            // Connect with the Database and import schema
+            $databaseHelper
+                ->connect('mysql:host=' . $database['host'] . ';dbname=' . $database['dbname'], $database['user'], $database['password']);
+
+            if (file_exists($sqlDumpPath)) {
+                $databaseHelper
+                    ->load(file_get_contents($sqlDumpPath));
+            } else {
+                throw new \Exception("Sql dump can't be found. Looking for this file: " . $sqlDumpPath, 1);
+            }
+        }
+    }
 }
