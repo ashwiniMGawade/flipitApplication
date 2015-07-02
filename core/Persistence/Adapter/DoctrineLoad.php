6731ba4446f39689fc1057375b4a3a210243f35a
Kuldeep Raj <kraj@seasiainfotech.com>
2015-07-02 18:00:44 +0530

    Create Doctrine adaper for entity manager

--- core/Persistence/Adapter/DoctrineLoad.php
+++ core/Persistence/Adapter/DoctrineLoad.php
@@ -7,15 +7,28 @@ use \Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use \Doctrine\Common\Annotations\AnnotationReader;
 use \Doctrine\Common\Annotations\AnnotationRegistry;
 
-class DoctrineLoad
+class DoctrineLoad 
 {
     protected $localeEntityManger;
     protected $userEntityManger;
-    public function __construct($dbParamsLocale, $dbParamsUser)
+    public function __construct($dbParamsLocale, $dbParamsUser, $appMode = 'development', $memcacheHostParams = 'localhost:11211')
     {
-        $paths = array('/../../../../core/Domain/Entity');
-        $isDevMode = true;
-        $config = \Doctrine\ORM\Tools\Setup::createConfiguration($isDevMode);
+        $paths = array('/../../../../core/Domain/Entity', '/../../../../core/Domain/Entity/User');
+        $splitMemcacheValues = explode(':', $memcacheHostParams);
+        $memcachePort = isset($splitMemcacheValues[1]) ? $splitMemcacheValues[1] : '';
+        $memcacheHost = isset($splitMemcacheValues[0]) ? $splitMemcacheValues[0] : '';
+        $memcache = new \Memcached();
+        $memcache->addServer($memcacheHost, $memcachePort);
+        $cache = new \Doctrine\Common\Cache\MemcachedCache;
+        $cache->setMemcached($memcache);
+        $isDevMode = false;
+        $proxyPath = null;
+        if ($appMode === 'development' || $appMode === 'testing') {
+            $cache = null;
+            $isDevMode = true;
+        }
+        $config = Setup::createConfiguration($isDevMode, $proxyPath, $cache);
+        $config->setProxyNamespace('Proxy');
         $driver = new AnnotationDriver(new AnnotationReader(), $paths);
         AnnotationRegistry::registerLoader('class_exists');
         $config->setMetadataDriverImpl($driver);
