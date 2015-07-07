b63d3cb8b76034bcef4997d632ca1887c6e57014
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-07 20:56:58 +0530

    KC-6606 #comment Creating Factory methods to return Create and APiKeyGenerator objects

--- core/Domain/Factory/AdministratorFactory.php
+++ core/Domain/Factory/AdministratorFactory.php
@@ -1,19 +1,39 @@
 <?php
 namespace Core\Domain\Factory;
 
+use Core\Domain\Service\ApiKeyGenerator;
 use Core\Domain\Usecase\Admin\GetApiKeyListingUsecase;
 use Core\Domain\Usecase\Admin\CreateApiKeyUsecase;
 use Core\Persistence\Factory\RepositoryFactory;
 
+/**
+ * Class AdministratorFactory
+ *
+ * @package Core\Domain\Factory
+ */
 class AdministratorFactory
 {
+    /**
+     * @return \Core\Domain\Usecase\Admin\GetApiKeyListingUsecase
+     */
     public static function getApiKeys()
     {
         return new GetApiKeyListingUsecase(RepositoryFactory::apiKeys());
     }
 
-    public static function addApiKey()
+    /**
+     * @return \Core\Domain\Usecase\Admin\CreateApiKeyUsecase
+     */
+    public static function createApiKey()
     {
         return new CreateApiKeyUsecase(RepositoryFactory::apiKeys());
+    }
+
+    /**
+     * @return \Core\Domain\Service\ApiKeyGenerator
+     */
+    public static function apiKey()
+    {
+        return new ApiKeyGenerator();
     }
 }
