92275be3ebe7a8a1f29231d83d084c2e78ef90ef
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-07 20:59:47 +0530

    KC-6606 #comment Adding Persist method to APiKeyREpo

--- core/Persistence/Database/Repository/ApiKeyRepository.php
+++ core/Persistence/Database/Repository/ApiKeyRepository.php
@@ -3,11 +3,19 @@ namespace Core\Persistence\Database\Repository;
 
 use Core\Domain\Repository\ApiKeyRepositoryInterface;
 
+/**
+ * Class ApiKeyRepository
+ *
+ * @package Core\Persistence\Database\Repository
+ */
 class ApiKeyRepository extends BaseRepository implements ApiKeyRepositoryInterface
 {
+    /**
+     * @param $entity
+     */
     public function persist($entity)
     {
-        $this->em->persist($entity);
+        $this->em->merge($entity);
         $this->em->flush();
     }
 }
