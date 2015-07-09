ac84610c78d706d777403df0863ec97dcb78090c
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-09 18:45:24 +0530

    KC-6606 #comment Modifying usecase to only return entity object, deleting test

--- core/Domain/Usecase/Admin/CreateApiKeyUsecase.php
+++ core/Domain/Usecase/Admin/CreateApiKeyUsecase.php
@@ -1,8 +1,7 @@
 <?php
 namespace Core\Domain\Usecase\Admin;
 
-use \Core\Domain\Repository\ApiKeyRepositoryInterface;
-use \Core\Domain\Entity\User\ApiKey;
+use Core\Domain\Entity\User\ApiKey;
 
 /**
  * Class CreateApiKeyUsecase
@@ -11,35 +10,8 @@ use \Core\Domain\Entity\User\ApiKey;
  */
 class CreateApiKeyUsecase
 {
-    /**
-     * @var \Core\Domain\Repository\ApiKeyRepositoryInterface
-     */
-    private $apiKeyRepository;
-
-    /**
-     * @param \Core\Domain\Repository\ApiKeyRepositoryInterface $apiKeyRepository
-     */
-    public function __construct(ApiKeyRepositoryInterface $apiKeyRepository)
+    public function execute()
     {
-        $this->apiKeyRepository = $apiKeyRepository;
-    }
-
-    /**
-     * @param $properties
-     *
-     * @return mixed
-     * @throws \Exception
-     */
-    public function execute($properties)
-    {
-        if (empty($properties)) {
-            throw new \Exception('The Properties cannot be empty');
-        }
-        $apiKey = new ApiKey();
-        $apiKey->__set('api_key', $properties['api_key']);
-        $apiKey->__set('user_id', $properties['user']);
-        $apiKey->__set('created_at', $properties['created_at']);
-        $apiKey->__set('deleted', $properties['deleted']);
-        return $this->apiKeyRepository->persist($apiKey);
+        return new ApiKey();
     }
 }
