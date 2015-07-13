1c16168347697f21b33877d63ef807363753368e
Ramesh Mhetre <ramesh@web-flight.nl>
2015-07-13 11:59:32 +0530

    KC-4666 #comment Changed in gitignore to restrict api/logs changed from git & some minor changes to getshops usecase

--- core/Domain/Usecase/Admin/GetShopUsecase.php
+++ core/Domain/Usecase/Admin/GetShopUsecase.php
@@ -15,13 +15,13 @@ class GetShopUsecase
     public function execute($id)
     {
         if (is_null($id) || !is_numeric($id)) {
-            throw new \Exception('Invalid shop Id', 404);
+            throw new \Exception('Invalid shop Id', 400);
         }
         
         $shop = $this->shopRepository->find('\Core\Domain\Entity\Shop', $id);
 
         if (false === is_object($shop)) {
-            throw new \Exception('Shop not found', 400);
+            throw new \Exception('Shop not found', 404);
         }
         return $shop;
     }
