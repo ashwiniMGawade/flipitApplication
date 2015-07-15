df78b80feb4755f508a4145397f4097d7fa48b6b
Ramesh Mhetre <ramesh@web-flight.nl>
2015-07-15 12:13:17 +0530

    KC-6646 #coment Improved code coverage

--- tests/domain/Usecase/Admin/GetShopUsecaseTest.php
+++ tests/domain/Usecase/Admin/GetShopUsecaseTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Usecase\Admin;
 
+use \Core\Domain\Entity\Shop;
 use \Core\Domain\Usecase\Admin\GetShopUsecase;
 
 class GetShopsUsecaseTest extends \Codeception\TestCase\Test
@@ -10,15 +11,30 @@ class GetShopsUsecaseTest extends \Codeception\TestCase\Test
      */
     protected $tester;
 
-    public function testGetShopUsecase()
+    public function testGetShopUsecaseWithIdNotExist()
     {
-        $id = 1;
+        $id = 0;
         $shopRepositoryMock = $this->createShopRepositoryMock();
         $shopRepositoryMock
             ->expects($this->once())
             ->method('find')
-            ->with($this->equalTo('\Core\Domain\Entity\Shop'), $this->equalTo(1));
+            ->with($this->equalTo('\Core\Domain\Entity\Shop'), $this->equalTo(0));
         $this->setExpectedException('Exception', 'Shop not found');
+        $shopUsecase = new GetShopUsecase($shopRepositoryMock);
+        $shopUsecase->execute($id);
+    }
+
+    public function testGetShopUsecase()
+    {
+        $id = 1;
+        $shop = new Shop();
+        $shop->__set('id', 1);
+        $shopRepositoryMock = $this->createShopRepositoryMock();
+        $shopRepositoryMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($this->equalTo('\Core\Domain\Entity\Shop'), $this->equalTo(1))
+            ->willReturn($shop);
         $shopUsecase = new GetShopUsecase($shopRepositoryMock);
         $shopUsecase->execute($id);
     }
