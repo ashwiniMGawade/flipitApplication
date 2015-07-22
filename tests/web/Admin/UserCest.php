8f4337600f561241ff61b05ade8e5ddd580a3cbd
Kim Pellikaan <kim@web-flight.nl>
2015-07-22 22:42:59 +0200

    KC-6747 #comment Updated the seed in such a way that the user is added properly. 1. Role constraint was not met. 2. website constraint was not met. 3. password reset time was not set.

--- tests/web/Admin/UserCest.php
+++ tests/web/Admin/UserCest.php
@@ -7,6 +7,9 @@ class UserCest
 {
     public function superAdminCanEditOtherSuperAdminTest(WebTester $I, \Codeception\Scenario $scenario)
     {
+        $date = new \DateTime('now');
+        $passwordChangeTime =  $date->format('Y-m-d H:m:s');
+
         $I = new WebTester\AdminSteps($scenario);
         $I->haveInDatabasePDOUser(
             'user',
@@ -17,9 +20,10 @@ class UserCest
                     'email' => 'superadmin1@flipit.com',
                     'password' => md5('Mind@123'),
                     'status' => 1,
-                    'roleid' => 1,
+                    'roleid' => 5,
                     'slug' => 'test-user-super123',
-                    'popularKortingscode' => '0'
+                    'popularKortingscode' => '0',
+                    'passwordchangetime' => $passwordChangeTime
                 ),
                 array(
                     'firstName' => 'superadmin2',
@@ -27,15 +31,29 @@ class UserCest
                     'email' => 'superadmin2@flipit.com',
                     'password' => md5('Mind@123'),
                     'status' => 1,
-                    'roleid' => 1,
+                    'roleid' => 5,
                     'slug' => 'test-user-super',
-                    'popularKortingscode' => '0'
+                    'popularKortingscode' => '0',
+                    'passwordchangetime' => $passwordChangeTime
+                )
+            )
+        );
+        $I->haveInDatabasePDOUser(
+            'ref_user_website',
+            array(
+                array(
+                    'userid' => '355',
+                    'websiteid' => '39'
+                ),
+                array(
+                    'userid' => '356',
+                    'websiteid' => '39'
                 )
             )
         );
         $I->login('superadmin1@flipit.com', 'Mind@123');
-        $I->amOnPage('admin/');
-        $I->amOnPage('admin/user/');
-        $I->see('superadmin2@flipit.com');
+        $I->see('superadmin1');
+        // $I->click('gebruikers');
+        // $I->see('superadmin2@flipit.com');
     }
 }
