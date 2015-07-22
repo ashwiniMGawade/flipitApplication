baa32e81062f0f360f2dcff1a86d7612d2e44056
Kim Pellikaan <kim@web-flight.nl>
2015-07-22 22:37:09 +0200

    KC-6747 #comment Made the login dynamic.

--- tests/web/_steps/AdminSteps.php
+++ tests/web/_steps/AdminSteps.php
@@ -1,16 +1,18 @@
 <?php
 namespace WebTester;
 
+use KC\Repository\User;
+
 class AdminSteps extends \WebTester
 {
-    public function login()
+    public function login($username = 'test@flipit.com', $password = 'password')
     {
         $I = $this;
         $config = \Codeception\Configuration::config();
-        $this->addUser();
+        // $this->addUser();
         $I->amOnPage(\LoginPage::$URL);
-        $I->fillField(\LoginPage::$usernameField, 'test@flipit.com');
-        $I->fillField(\LoginPage::$passwordField, 'password');
+        $I->fillField(\LoginPage::$usernameField, $username);
+        $I->fillField(\LoginPage::$passwordField, $password);
         $I->click(\LoginPage::$formSubmitButton);
     }
 
@@ -21,32 +23,31 @@ class AdminSteps extends \WebTester
 
     protected function addUser()
     {
-        $userParameters = array(
-            'firstName' => 'test',
-            'lastName' => 'user',
-            'email' => 'test@flipit.com',
-            'password' => 'Mind@123',
-            'status' => 1,
-            'role' => 1,
-            'slug' => 'test-user',
-            'imageName' => 'test.png',
-            'locale' => '',
-            'nameStatus' => '0',
-            'google' => '',
-            'twitter' => '',
-            'pintrest' => '',
-            'likes' => '',
-            'dislike' => '',
-            'editortext' => '',
-            'popularKortingscode' => '0'
-        );
-        /*$user = new \KC\Repository\User();
-        //$user->truncateTables();
-        $role = new \KC\Repository\Role();
-        $role->addUserRoles();
-        $rights = new \KC\Repository\Rights();
-        $rights->addRights();
-        $user->addUser($userParameters, 'test.png');
-        //die('Hello');*/
+        //$userParameters = array(
+        //    'firstName' => 'test',
+        //    'lastName' => 'user',
+        //    'email' => 'test@flipit.com',
+        //    'password' => 'Mind@123',
+        //    'status' => 1,
+        //    'role' => 1,
+        //    'slug' => 'test-user',
+        //    'imageName' => 'test.png',
+        //    'locale' => '',
+        //    'nameStatus' => '0',
+        //    'google' => '',
+        //    'twitter' => '',
+        //    'pintrest' => '',
+        //    'likes' => '',
+        //    'dislike' => '',
+        //    'editortext' => '',
+        //    'popularKortingscode' => '0'
+        //);
+        // $user = new User();
+        // $user->truncateTables();
+        // $role = new \KC\Repository\Role();
+        // $role->addUserRoles();
+        // $rights = new \KC\Repository\Rights();
+        // $rights->addRights();
+        // $user->addUser($userParameters, 'test.png');
     }
 }
