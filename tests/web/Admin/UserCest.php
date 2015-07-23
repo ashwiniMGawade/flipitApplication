d549d3b63972d29bfe24a4f144317eca37dd1d7a
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-23 18:00:47 +0530

    KC-6747 #comment Adding the tests to check permissions of Amdin and Editor users

--- tests/web/Admin/UserCest.php
+++ tests/web/Admin/UserCest.php
@@ -64,4 +64,150 @@ class UserCest
         $I->waitForElementVisible('#userList tbody tr', 20);
         $I->see('NewFirstName');
     }
+
+    public function userShouldNotSeeThesePagesWhenRoleEqualsAdminTest(WebTester $I, \Codeception\Scenario $scenario)
+    {
+        $date = new \DateTime('now');
+        $passwordChangeTime =  $date->format('Y-m-d H:m:s');
+
+        $I = new WebTester\AdminSteps($scenario);
+        $I->haveInDatabasePDOUser(
+            'user',
+            array(
+                array(
+                    'firstName' => 'admin',
+                    'lastName' => 'user',
+                    'email' => 'admin@flipit.com',
+                    'password' => md5('Mind@123'),
+                    'status' => 1,
+                    'roleid' => 2,
+                    'slug' => 'test-admin-123',
+                    'popularKortingscode' => '0',
+                    'passwordchangetime' => $passwordChangeTime
+                )
+            )
+        );
+        $I->haveInDatabasePDOUser(
+            'ref_user_website',
+            array(
+                array(
+                    'userid' => '365',
+                    'websiteid' => '1'
+                )
+            )
+        );
+
+        $I->login('admin@flipit.com', 'Mind@123');
+        $I->see('admin');
+
+        #Admin does not have access to Robots page
+        $I->amOnPage('admin/robot');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Chains page
+        $I->amOnPage('admin/chain');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Locale Settings page
+        $I->amOnPage('admin/locale/locale-settings');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Email Settings page
+        $I->amOnPage('admin/email/email-settings');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Import Shops page
+        $I->amOnPage('admin/shop/importshops');
+        $I->see('Voeg nieuwe winkel toe');
+        #Admin does not have access to Visitors page
+        $I->amOnPage('admin/visitor');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Ip Addresses page
+        $I->amOnPage('admin/ipaddresses');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to Users page
+        $I->amOnPage('admin/user');
+        $I->see('Scores of the last 7 days');
+        #Admin does not have access to ApiKeys page
+        $I->amOnPage('admin/apikeys');
+        $I->see('Scores of the last 7 days');
+    }
+
+    public function userShouldNotSeeThesePagesWhenRoleEqualsEditorTest(WebTester $I, \Codeception\Scenario $scenario)
+    {
+        $date = new \DateTime('now');
+        $passwordChangeTime =  $date->format('Y-m-d H:m:s');
+
+        $I = new WebTester\AdminSteps($scenario);
+        $I->haveInDatabasePDOUser(
+            'user',
+            array(
+                array(
+                    'firstName' => 'Editor',
+                    'lastName' => 'user',
+                    'email' => 'editor@flipit.com',
+                    'password' => md5('Mind@123'),
+                    'status' => 1,
+                    'roleid' => 4,
+                    'slug' => 'test-editor-123',
+                    'popularKortingscode' => '0',
+                    'passwordchangetime' => $passwordChangeTime
+                )
+            )
+        );
+        $I->haveInDatabasePDOUser(
+            'ref_user_website',
+            array(
+                array(
+                    'userid' => '365',
+                    'websiteid' => '1'
+                )
+            )
+        );
+
+        $I->login('editor@flipit.com', 'Mind@123');
+        $I->see('Editor');
+
+        #Editor does not have access to Robots page
+        $I->amOnPage('admin/robot');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Chains page
+        $I->amOnPage('admin/chain');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Locale Settings page
+        $I->amOnPage('admin/locale/locale-settings');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Email Settings page
+        $I->amOnPage('admin/email/email-settings');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Import Shops page
+        $I->amOnPage('admin/shop/importshops');
+        $I->see('Voeg nieuwe winkel toe');
+        #Editor does not have access to Visitors page
+        $I->amOnPage('admin/visitor');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Ip Addresses page
+        $I->amOnPage('admin/ipaddresses');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Users page
+        $I->amOnPage('admin/user');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to ApiKeys page
+        $I->amOnPage('admin/apikeys');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Account Settings page
+        $I->amOnPage('admin/accountsetting');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Email Management page
+        $I->amOnPage('admin/accountsetting/emailcontent');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Footer page
+        $I->amOnPage('admin/footer');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Redirect page
+        $I->amOnPage('admin/redirect');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Language page
+        $I->amOnPage('admin/language');
+        $I->see('Scores of the last 7 days');
+        #Editor does not have access to Splash page
+        $I->amOnPage('admin/splash');
+        $I->see('Scores of the last 7 days');
+    }
 }
