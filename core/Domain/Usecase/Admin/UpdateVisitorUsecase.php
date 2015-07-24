2838adda331a8c89950ad1ca4e2a483d5d36340f
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-24 16:50:53 +0530

    KC-3398 #comment Updating theUpdate Visitor Usecase and test to save the visitor

--- core/Domain/Usecase/Admin/UpdateVisitorUsecase.php
+++ core/Domain/Usecase/Admin/UpdateVisitorUsecase.php
@@ -1,32 +1,46 @@
 <?php
 namespace Core\Domain\Usecase\Admin;
 
+use \Core\Domain\Repository\VisitorRepositoryInterface;
+
 class UpdateVisitorUsecase
 {
-    public function execute($parameters)
+    protected $visitorRepository;
+
+    public function __construct(VisitorRepositoryInterface $visitorRepository)
     {
-        if (!is_array($parameters) || empty($parameters)) {
-            throw new \Exception('Invalid Parameters');
-        }
-        if (!isset($parameters['email']) || !isset($parameters['event'])) {
-            throw new \Exception('Invalid Parameters');
-        }
-        if (!$this->validateEventName($parameters['event'])) {
-            throw new \Exception('Invalid Event');
-        }
+        $this->visitorRepository = $visitorRepository;
     }
 
-    private function validateEventName($eventName)
+    public function execute($parameters)
     {
-        $validMandrillEvents = array(
-            'open',
-            'click',
-            'soft_bounce',
-            'hard_bounce'
-        );
-        if (!in_array($eventName, $validMandrillEvents, true)) {
-            return false;
+        if (!is_array($parameters) || empty($parameters) || !isset($parameters['email']) || !isset($parameters['event'])) {
+            throw new \Exception('Invalid Parameters');
         }
-        return true;
+        if (!$visitor = $this->visitorRepository->findOneBy('\Core\Domain\Entity\Visitor', array('email' => $parameters['email']))) {
+            throw new \Exception('Invalid Email');
+        }
+        switch ($parameters['event']) {
+            case 'open':
+                $openCount = (int) $visitor->getMailOpenCount();
+                $visitor->setMailOpenCount($openCount + 1);
+                break;
+            case 'click':
+                $clickCount = (int) $visitor->getMailClickCount();
+                $visitor->setMailClickCount($clickCount + 1);
+                break;
+            case 'soft_bounce':
+                $softBounceCount = (int) $visitor->getMailSoftBounceCount();
+                $visitor->setMailSoftBounceCount($softBounceCount + 1);
+                break;
+            case 'hard_bounce':
+                $hardBounceCount = (int) $visitor->getMailHardBounceCount();
+                $visitor->setMailHardBounceCount($hardBounceCount + 1);
+                break;
+            default:
+                throw new \Exception('Invalid Event');
+                break;
+        }
+        return $this->visitorRepository->save($visitor);
     }
 }
--- tests/domain/Usecase/Admin/UpdateVisitorUsecaseTest.php
+++ tests/domain/Usecase/Admin/UpdateVisitorUsecaseTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Usecase\Admin;
 
+use Core\Domain\Entity\Visitor;
 use \Core\Domain\Usecase\Admin\UpdateVisitorUsecase;
 
 class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
@@ -10,25 +11,142 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
      */
     protected $tester;
 
+    protected $visitorRepository;
+
+    protected function _before()
+    {
+        $this->visitorRepository = $this->createVisitorRepositoryInterfaceMock();
+    }
+
     public function testUpdateVisitorUsecaseWhenInputNotEqualsArray()
     {
         $this->setExpectedException('Exception', 'Invalid Parameters');
         $invalidInput = 'NOT_ARRAY';
-        (new UpdateVisitorUsecase())->execute($invalidInput);
+        (new UpdateVisitorUsecase($this->visitorRepository))->execute($invalidInput);
     }
 
     public function testUpdateVisitorUsecaseWhenInputEqualsEmptyArray()
     {
         $this->setExpectedException('Exception', 'Invalid Parameters');
         $invalidInput = array();
-        (new UpdateVisitorUsecase())->execute($invalidInput);
+        (new UpdateVisitorUsecase($this->visitorRepository))->execute($invalidInput);
     }
 
-    public function testUpdateVisitorUsecaseWhenInputArrayIsValid()
+    public function testUpdateVisitorUsecaseWhenEmailIsInValid()
     {
-//        $validInput = array(
-//            'test@example.com' => 'click'
-//        );
-//        (new UpdateVisitorUsecase())->execute($validInput);
+        $this->setExpectedException('Exception', 'Invalid Email');
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'open'
+        );
+        (new UpdateVisitorUsecase($this->visitorRepository))->execute($validInput);
+    }
+
+    public function testUsecaseIncrementsOpenCountWhenTryingToUpdateEmailOpenForAnEmailId()
+    {
+        $visitor = $this->createVisitorMock(1);
+        $initialOpenCount = $visitor->getMailOpenCount();
+        $visitor->expects($this->once())->method('getMailOpenCount');
+        $visitor->expects($this->once())->method('setMailOpenCount')->with($initialOpenCount + 1);
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'open'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
+    public function testUsecaseIncrementsClickCountWhenTryingToUpdateEmailClickForAnEmailId()
+    {
+        $visitor = $this->createVisitorMock(1);
+        $initialClickCount = $visitor->getMailClickCount();
+        $visitor->expects($this->once())->method('getMailClickCount');
+        $visitor->expects($this->once())->method('setMailClickCount')->with($initialClickCount + 1);
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'click'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
+    public function testUsecaseIncrementsSoftBounceCountWhenTryingToUpdateEmailSoftBounceForAnEmailId()
+    {
+        $visitor = $this->createVisitorMock(1);
+        $initialSoftBounceCount = $visitor->getMailSoftBounceCount();
+        $visitor->expects($this->once())->method('getMailSoftBounceCount');
+        $visitor->expects($this->once())->method('setMailSoftBounceCount')->with($initialSoftBounceCount + 1);
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'soft_bounce'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
+    public function testUsecaseIncrementsHardBounceCountWhenTryingToUpdateEmailHardBounceForAnEmailId()
+    {
+        $visitor = $this->createVisitorMock(1);
+        $initialHardBounceCount = $visitor->getMailHardBounceCount();
+        $visitor->expects($this->once())->method('getMailHardBounceCount');
+        $visitor->expects($this->once())->method('setMailHardBounceCount')->with($initialHardBounceCount + 1);
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'hard_bounce'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
+    public function testUpdateVisitorUsecaseThrowsExceptionWhenEventIsInValid()
+    {
+        $visitor = $this->createVisitorMock(1);
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+
+        $this->setExpectedException('Exception', 'Invalid Event');
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'invalid_event'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
+    private function createVisitorMock($id)
+    {
+        $visitor = $this->getMock('\Core\Domain\Entity\Visitor');
+        $visitor->setId($id);
+        $visitor->setMailOpenCount(1);
+        $visitor->setMailClickCount(1);
+        $visitor->setMailSoftBounceCount(1);
+        $visitor->setMailHardBounceCount(1);
+        return $visitor;
+    }
+
+    private function createVisitorRepositoryInterfaceMock()
+    {
+        return $this->getMock('\Core\Domain\Repository\VisitorRepositoryInterface');
+    }
+
+    private function createVisitorRepositoryWithFindOneByMethodMock($returns)
+    {
+        $visitorRepository = $this->createVisitorRepositoryInterfaceMock();
+        $visitorRepository->expects($this->once())
+                          ->method('findOneBy')
+                          ->with($this->isType('string', '\Core\Domain\Entity\Visitor'), $this->isType('array'))
+                          ->willReturn($returns);
+        return $visitorRepository;
     }
 }
