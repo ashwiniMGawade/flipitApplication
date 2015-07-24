9902d58b5e82c2c063aa04dbc7537c145e9b004a
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-24 14:37:05 +0530

    KC-3398 #comment Adding getters and Setters to the Visitors Entity

--- core/Domain/Entity/Visitor.php
+++ core/Domain/Entity/Visitor.php
@@ -206,6 +206,614 @@ class Visitor
      */
     protected $mailSoftBounceCount;
 
+    /**
+     * @return mixed
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param mixed $active
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActiveCodeid()
+    {
+        return $this->active_codeid;
+    }
+
+    /**
+     * @param mixed $active_codeid
+     */
+    public function setActiveCodeid($active_codeid)
+    {
+        $this->active_codeid = $active_codeid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChangepasswordrequest()
+    {
+        return $this->changepasswordrequest;
+    }
+
+    /**
+     * @param mixed $changepasswordrequest
+     */
+    public function setChangepasswordrequest($changepasswordrequest)
+    {
+        $this->changepasswordrequest = $changepasswordrequest;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodeAlert()
+    {
+        return $this->codeAlert;
+    }
+
+    /**
+     * @param mixed $codeAlert
+     */
+    public function setCodeAlert($codeAlert)
+    {
+        $this->codeAlert = $codeAlert;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodeAlertSendDate()
+    {
+        return $this->code_alert_send_date;
+    }
+
+    /**
+     * @param mixed $code_alert_send_date
+     */
+    public function setCodeAlertSendDate($code_alert_send_date)
+    {
+        $this->code_alert_send_date = $code_alert_send_date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConversions()
+    {
+        return $this->conversions;
+    }
+
+    /**
+     * @param mixed $conversions
+     */
+    public function setConversions($conversions)
+    {
+        $this->conversions = $conversions;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param mixed $createdBy
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentLogIn()
+    {
+        return $this->currentLogIn;
+    }
+
+    /**
+     * @param mixed $currentLogIn
+     */
+    public function setCurrentLogIn($currentLogIn)
+    {
+        $this->currentLogIn = $currentLogIn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @param mixed $dateOfBirth
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @param mixed $deleted
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFashionNewsLetter()
+    {
+        return $this->fashionNewsLetter;
+    }
+
+    /**
+     * @param mixed $fashionNewsLetter
+     */
+    public function setFashionNewsLetter($fashionNewsLetter)
+    {
+        $this->fashionNewsLetter = $fashionNewsLetter;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFavoriteOffer()
+    {
+        return $this->favoriteOffer;
+    }
+
+    /**
+     * @param mixed $favoriteOffer
+     */
+    public function setFavoriteOffer($favoriteOffer)
+    {
+        $this->favoriteOffer = $favoriteOffer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFavoriteshops()
+    {
+        return $this->favoriteshops;
+    }
+
+    /**
+     * @param mixed $favoriteshops
+     */
+    public function setFavoriteshops($favoriteshops)
+    {
+        $this->favoriteshops = $favoriteshops;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFavoritevisitorshops()
+    {
+        return $this->favoritevisitorshops;
+    }
+
+    /**
+     * @param mixed $favoritevisitorshops
+     */
+    public function setFavoritevisitorshops($favoritevisitorshops)
+    {
+        $this->favoritevisitorshops = $favoritevisitorshops;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInterested()
+    {
+        return $this->interested;
+    }
+
+    /**
+     * @param mixed $interested
+     */
+    public function setInterested($interested)
+    {
+        $this->interested = $interested;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastLogIn()
+    {
+        return $this->lastLogIn;
+    }
+
+    /**
+     * @param mixed $lastLogIn
+     */
+    public function setLastLogIn($lastLogIn)
+    {
+        $this->lastLogIn = $lastLogIn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailClickCount()
+    {
+        return $this->mailClickCount;
+    }
+
+    /**
+     * @param mixed $mailClickCount
+     */
+    public function setMailClickCount($mailClickCount)
+    {
+        $this->mailClickCount = $mailClickCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailHardBounceCount()
+    {
+        return $this->mailHardBounceCount;
+    }
+
+    /**
+     * @param mixed $mailHardBounceCount
+     */
+    public function setMailHardBounceCount($mailHardBounceCount)
+    {
+        $this->mailHardBounceCount = $mailHardBounceCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailOpenCount()
+    {
+        return $this->mailOpenCount;
+    }
+
+    /**
+     * @param mixed $mailOpenCount
+     */
+    public function setMailOpenCount($mailOpenCount)
+    {
+        $this->mailOpenCount = $mailOpenCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMailSoftBounceCount()
+    {
+        return $this->mailSoftBounceCount;
+    }
+
+    /**
+     * @param mixed $mailSoftBounceCount
+     */
+    public function setMailSoftBounceCount($mailSoftBounceCount)
+    {
+        $this->mailSoftBounceCount = $mailSoftBounceCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOffer()
+    {
+        return $this->offer;
+    }
+
+    /**
+     * @param mixed $offer
+     */
+    public function setOffer($offer)
+    {
+        $this->offer = $offer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param mixed $postalCode
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfileImg()
+    {
+        return $this->profile_img;
+    }
+
+    /**
+     * @param mixed $profile_img
+     */
+    public function setProfileImg($profile_img)
+    {
+        $this->profile_img = $profile_img;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPwd()
+    {
+        return $this->pwd;
+    }
+
+    /**
+     * @param mixed $pwd
+     */
+    public function setPwd($pwd)
+    {
+        $this->pwd = $pwd;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTravelNewsLetter()
+    {
+        return $this->travelNewsLetter;
+    }
+
+    /**
+     * @param mixed $travelNewsLetter
+     */
+    public function setTravelNewsLetter($travelNewsLetter)
+    {
+        $this->travelNewsLetter = $travelNewsLetter;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVisitorKeyword()
+    {
+        return $this->visitorKeyword;
+    }
+
+    /**
+     * @param mixed $visitorKeyword
+     */
+    public function setVisitorKeyword($visitorKeyword)
+    {
+        $this->visitorKeyword = $visitorKeyword;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVisitorimage()
+    {
+        return $this->visitorimage;
+    }
+
+    /**
+     * @param mixed $visitorimage
+     */
+    public function setVisitorimage($visitorimage)
+    {
+        $this->visitorimage = $visitorimage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeeklyNewsLetter()
+    {
+        return $this->weeklyNewsLetter;
+    }
+
+    /**
+     * @param mixed $weeklyNewsLetter
+     */
+    public function setWeeklyNewsLetter($weeklyNewsLetter)
+    {
+        $this->weeklyNewsLetter = $weeklyNewsLetter;
+    }
+
     public function __get($property)
     {
         return $this->$property;
