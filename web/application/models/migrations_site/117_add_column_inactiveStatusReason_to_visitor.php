b6b58c151c3f09eb89555aa0997016a1afe80a9d
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-27 13:49:38 +0530

    KC-3398 #comment Adding a new column to the visiotrs table to store the reason for inactivity of a visitor

---
+++ web/application/models/migrations_site/117_add_column_inactiveStatusReason_to_visitor.php
@@ -0,0 +1,13 @@
+<?php
+class AddEmailDataColumnsVisitor extends Doctrine_Migration_Base
+{
+    public function up()
+    {
+        $this->addColumn('visitor', 'inactiveStatusReason', 'varchar', 20);
+    }
+
+    public function down()
+    {
+        $this->removeColumn('visitor', 'inactiveStatusReason');
+    }
+}
