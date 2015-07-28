9e1f7b9cd889959e130425573c15f518b54e48ac
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-28 12:35:30 +0530

    KC-3398 #comment Modifying the UpdateVisitorUsecaseTest

--- tests/domain/Usecase/Admin/UpdateVisitorUsecaseTest.php
+++ tests/domain/Usecase/Admin/UpdateVisitorUsecaseTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Usecase\Admin;
 
-use Core\Domain\Entity\Visitor;
+use \Core\Domain\Entity\Visitor;
 use \Core\Domain\Usecase\Admin\UpdateVisitorUsecase;
 
 class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
@@ -44,7 +44,7 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
 
     public function testUsecaseIncrementsOpenCountWhenTryingToUpdateEmailOpenForAnEmailId()
     {
-        $visitor = $this->createVisitorMock(1);
+        $visitor = $this->createVisitorMock();
         $initialOpenCount = $visitor->getMailOpenCount();
         $visitor->expects($this->once())->method('getMailOpenCount');
         $visitor->expects($this->once())->method('setMailOpenCount')->with($initialOpenCount + 1);
@@ -61,7 +61,7 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
 
     public function testUsecaseIncrementsClickCountWhenTryingToUpdateEmailClickForAnEmailId()
     {
-        $visitor = $this->createVisitorMock(1);
+        $visitor = $this->createVisitorMock();
         $initialClickCount = $visitor->getMailClickCount();
         $visitor->expects($this->once())->method('getMailClickCount');
         $visitor->expects($this->once())->method('setMailClickCount')->with($initialClickCount + 1);
@@ -78,7 +78,7 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
 
     public function testUsecaseIncrementsSoftBounceCountWhenTryingToUpdateEmailSoftBounceForAnEmailId()
     {
-        $visitor = $this->createVisitorMock(1);
+        $visitor = $this->createVisitorMock();
         $initialSoftBounceCount = $visitor->getMailSoftBounceCount();
         $visitor->expects($this->once())->method('getMailSoftBounceCount');
         $visitor->expects($this->once())->method('setMailSoftBounceCount')->with($initialSoftBounceCount + 1);
@@ -93,9 +93,28 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
         (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
     }
 
+    public function testUsecaseSetsTheVisitorAsInactiveWhenSoftBounceCountIsGreaterThanOrEqualTo6()
+    {
+        $initialSoftBounceCount = 5;
+        $visitor = $this->createVisitorWithGetMailSoftBounceCountMethodMock($initialSoftBounceCount);
+        $visitor->expects($this->once())->method('getMailSoftBounceCount');
+        $visitor->expects($this->once())->method('setMailSoftBounceCount')->with($initialSoftBounceCount + 1);
+        $visitor->expects($this->once())->method('setActive')->with(0);
+        $visitor->expects($this->once())->method('setInactiveStatusReason')->with('Soft Bounce');
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'soft_bounce'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
     public function testUsecaseIncrementsHardBounceCountWhenTryingToUpdateEmailHardBounceForAnEmailId()
     {
-        $visitor = $this->createVisitorMock(1);
+        $visitor = $this->createVisitorMock();
         $initialHardBounceCount = $visitor->getMailHardBounceCount();
         $visitor->expects($this->once())->method('getMailHardBounceCount');
         $visitor->expects($this->once())->method('setMailHardBounceCount')->with($initialHardBounceCount + 1);
@@ -110,9 +129,28 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
         (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
     }
 
+    public function testUsecaseSetsTheVisitorAsInactiveWhenHardBounceCountIsGreaterThanOrEqualTo3()
+    {
+        $initialHardBounceCount = 2;
+        $visitor = $this->createVisitorWithGetMailHardBounceCountMethodMock($initialHardBounceCount);
+        $visitor->expects($this->once())->method('getMailHardBounceCount');
+        $visitor->expects($this->once())->method('setMailHardBounceCount')->with($initialHardBounceCount + 1);
+        $visitor->expects($this->once())->method('setActive')->with(0);
+        $visitor->expects($this->once())->method('setInactiveStatusReason')->with('Hard Bounce');
+
+        $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
+        $validatorRepository->expects($this->once())->method('save')->with($this->isInstanceOf('\Core\Domain\Entity\Visitor'));
+
+        $validInput = array(
+            'email' => 'test@example.com',
+            'event' => 'hard_bounce'
+        );
+        (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
+    }
+
     public function testUpdateVisitorUsecaseThrowsExceptionWhenEventIsInValid()
     {
-        $visitor = $this->createVisitorMock(1);
+        $visitor = $this->createVisitorMock();
 
         $validatorRepository = $this->createVisitorRepositoryWithFindOneByMethodMock($visitor);
 
@@ -124,14 +162,23 @@ class UpdateVisitorUsecaseTest extends \Codeception\TestCase\Test
         (new UpdateVisitorUsecase($validatorRepository))->execute($validInput);
     }
 
-    private function createVisitorMock($id)
+    private function createVisitorMock()
     {
         $visitor = $this->getMock('\Core\Domain\Entity\Visitor');
-        $visitor->setId($id);
-        $visitor->setMailOpenCount(1);
-        $visitor->setMailClickCount(1);
-        $visitor->setMailSoftBounceCount(1);
-        $visitor->setMailHardBounceCount(1);
+        return $visitor;
+    }
+
+    private function createVisitorWithGetMailSoftBounceCountMethodMock($count)
+    {
+        $visitor = $this->createVisitorMock();
+        $visitor->expects($this->once())->method('getMailSoftBounceCount')->willReturn($count);
+        return $visitor;
+    }
+
+    private function createVisitorWithGetMailHardBounceCountMethodMock($count)
+    {
+        $visitor = $this->createVisitorMock();
+        $visitor->expects($this->once())->method('getMailHardBounceCount')->willReturn($count);
         return $visitor;
     }
 
