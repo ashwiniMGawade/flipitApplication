b8ddb2fcea924a8ec852ac93158db53332b22050
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-07-30 11:58:35 +0530

    KC-6888 #comment Removing the extra id for the exclusive field in the addOffer template

--- tests/web/Admin/OfferCest.php
+++ tests/web/Admin/OfferCest.php
@@ -21,6 +21,45 @@ class OfferCest
         $this->fillForm($I);
     }
 
+    public function editOffer(WebTester $I, \Codeception\Scenario $scenario)
+    {
+        $I->haveInDatabasePDOSite(
+            'offer',
+            array(
+                'title' => 'TEST OFFER',
+                'visability' => 'DE',
+                'discounttype' => 'CD',
+                'couponcode' => 'TESTOFFER',
+                'startdate' => '2015-07-22 14:42:03',
+                'enddate' => '2015-07-22 14:42:03',
+                'discountvalueType' => '',
+                'authorId' => '1',
+                'shopid' => '1',
+                'maxcode' => '0',
+                'deleted' => '0',
+                'created_at' => '2015-07-22 14:42:03',
+                'updated_at' => '2015-07-22 14:42:03',
+                'userGenerated' => '0',
+                'approved' => '0',
+                'offline' => '0',
+                'tilesId' => '1',
+                'shopexist' => '1',
+                'couponcodetype' => 'GN'
+            )
+        );
+
+        $I->amOnPage('admin/offer');
+        $I->waitForText('TESTOFFER');
+        $I->click('TESTOFFER');
+        $I->see('Aanbieding aanpassen');
+        $I->click('.liimg');
+        $I->click('#optionsOnbtn');
+        $I->click('Exclusieve code');
+        $I->click('#offerEndButtons button#updateOfferBtn');
+        $I->wait(10);
+        $I->see('De actie is succesvol aangepast!');
+    }
+
     protected function fillForm($I)
     {
         $I->click('Select a Shop');
@@ -30,6 +69,8 @@ class OfferCest
         $I->fillField('#addofferTitle', 'functional test');
         $I->fillField('#couponCodeTxt', 'FUN234');
         $I->click('.liimg');
-        $I->click('#addOfferBtn');
+        $I->click('#optionsOnbtn');
+        $I->click('Exclusieve code');
+        $I->click('#offerEndButtons button#addOfferBtn');
     }
 }
