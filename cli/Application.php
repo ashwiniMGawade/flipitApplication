8e7184d62a45fc4d6cd76a8ce0b34d6d33f2454c
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-03 20:50:07 +0530

    KC-6871 #comment Adding a new command to mark the visitors as inactive

--- cli/Application.php
+++ cli/Application.php
@@ -1,9 +1,12 @@
 #!/usr/bin/env php
 <?php
 set_time_limit(0);
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Console\Application;
+use \Command\GreetCommand;
+use \Command\Email\VisitorInactiveCommand;
 
 $app = new Application();
 $app->add(new GreetCommand());
+$app->add(new VisitorInactiveCommand());
 $app->run();
---
+++ cli/Command/Email/VisitorInactiveCommand.php
@@ -0,0 +1,23 @@
+<?php
+namespace Command\Email;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class VisitorInactiveCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('visitor:inactive')
+            ->setDescription('Inactivate the Visitors that have not opened the newsletters in the past 3 months');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Helloo');
+    }
+}
