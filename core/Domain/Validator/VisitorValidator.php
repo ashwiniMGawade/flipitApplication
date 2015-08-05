4bd28249fb2221d91cabe1b433f9bc61e9322a95
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-05 20:07:33 +0530

    KC-6871 #comment Adding validations on the email fields

--- core/Domain/Validator/VisitorValidator.php
+++ core/Domain/Validator/VisitorValidator.php
@@ -16,7 +16,24 @@ class VisitorValidator
     public function validate(Visitor $visitor)
     {
         $rules = array(
+            'mailClickCount' => array(
+                $this->validator->notNull(),
+                $this->validator->type(array('type' => 'integer'))
+            ),
+            'mailOpenCount' => array(
+                $this->validator->notNull(),
+                $this->validator->type(array('type' => 'integer'))
+            ),
+            'mailHardBounceCount' => array(
+                $this->validator->notNull(),
+                $this->validator->type(array('type' => 'integer'))
+            ),
+            'mailSoftBounceCount' => array(
+                $this->validator->notNull(),
+                $this->validator->type(array('type' => 'integer'))
+            ),
             'inactiveStatusReason' => array(
+                $this->validator->notNull(),
                 $this->validator->type(array('type' => 'string'))
             ),
             'active' => array(
@@ -24,8 +41,7 @@ class VisitorValidator
                 $this->validator->length(array('min' => 1, 'max' => 1))
             ),
             'lastEmailOpenDate' => array(
-                $this->validator->type(array('type' => 'integer')),
-                $this->validator->length(array('min' => 11, 'max' => 11))
+                $this->validator->notNull()
             )
         );
         return $this->validator->validate($visitor, $rules);
