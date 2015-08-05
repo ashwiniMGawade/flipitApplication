d5c884922b90b632d20b954c58a2396e576a03b8
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-05 18:57:03 +0530

    KC-6871 #comment Refactorng the VisitorValidator TEst

--- tests/domain/Validator/VisitorValidatorTest.php
+++ tests/domain/Validator/VisitorValidatorTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace Validator;
 
+use \Core\Domain\Entity\Visitor;
+use \Core\Domain\Validator\VisitorValidator;
+
 class VisitorValidatorTest extends \Codeception\TestCase\Test
 {
     /**
@@ -8,7 +11,26 @@ class VisitorValidatorTest extends \Codeception\TestCase\Test
      */
     protected $tester;
 
-    public function testApiKeyValidatorWithValidOutcome()
+    public function testVisitorValidatorWithValidOutcome()
     {
+        $visitorValidator = new VisitorValidator($this->mockValidatorInterface(true));
+        $this->assertTrue($visitorValidator->validate(new Visitor()));
+    }
+
+    public function testVisitorValidatorWithInvalidOutcome()
+    {
+        $visitorValidator = new VisitorValidator($this->mockValidatorInterface(false));
+        $this->assertFalse($visitorValidator->validate(new Visitor()));
+    }
+
+    private function mockValidatorInterface($flag)
+    {
+        $mockValidatorInterface = $this->getMock('\Core\Domain\Adapter\ValidatorInterface');
+        $mockValidatorInterface
+            ->expects($this->once())
+            ->method('validate')
+            ->with($this->isType('object'), $this->isType('array'))
+            ->willReturn($flag);
+        return $mockValidatorInterface;
     }
 }
