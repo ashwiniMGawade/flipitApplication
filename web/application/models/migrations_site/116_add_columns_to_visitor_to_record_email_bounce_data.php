e55c07053f086c2ba6910dbad9ec1f37f1c85838
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-17 17:22:16 +0530

    KC-3398 #comment Refactoring the migrations

--- web/application/models/migrations_site/116_add_columns_to_visitor_to_record_email_bounce_data.php
+++ web/application/models/migrations_site/116_add_columns_to_visitor_to_record_email_bounce_data.php
@@ -1,12 +1,14 @@
 <?php
-class AddEmailDataColumnsVisitor extends Doctrine_Migration_Base
+class AddEmailColumnsVisitor extends Doctrine_Migration_Base
 {
     public function up()
     {
-        $this->addColumn('visitor', 'mailClickCount', 'integer', 20);
-        $this->addColumn('visitor', 'mailOpenCount', 'integer', 20);
-        $this->addColumn('visitor', 'mailHardBounceCount', 'integer', 20);
-        $this->addColumn('visitor', 'mailSoftBounceCount', 'integer', 20);
+        $this->addColumn('visitor', 'mailClickCount', 'integer', '', array('notnull' => 1, 'default' => 0));
+        $this->addColumn('visitor', 'mailOpenCount', 'integer', '', array('notnull' => 1, 'default' => 0));
+        $this->addColumn('visitor', 'mailHardBounceCount', 'integer', '', array('notnull' => 1, 'default' => 0));
+        $this->addColumn('visitor', 'mailSoftBounceCount', 'integer', '', array('notnull' => 1, 'default' => 0));
+        $this->addColumn('visitor', 'inactiveStatusReason', 'varchar', 20);
+        $this->addColumn('visitor', 'lastEmailOpenDate', 'timestamp');
     }
 
     public function down()
@@ -15,5 +17,7 @@ class AddEmailDataColumnsVisitor extends Doctrine_Migration_Base
         $this->removeColumn('visitor', 'mailOpenCount');
         $this->removeColumn('visitor', 'mailHardBounceCount');
         $this->removeColumn('visitor', 'mailSoftBounceCount');
+        $this->removeColumn('visitor', 'inactiveStatusReason');
+        $this->removeColumn('visitor', 'lastEmailOpenDate');
     }
 }
