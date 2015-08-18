27d360ce1a628e66a7d44a3f587caa88c53d3d58
Kim Pellikaan <kim@web-flight.nl>
2015-08-18 13:43:03 +0200

    refactored the migrations a little so the work until they get replaced

--- web/application/migration/migrations_user.php
+++ web/application/migration/migrations_user.php
@@ -3,33 +3,40 @@
 require_once 'ConstantForMigration.php';
 require_once('CommonMigrationFunctions.php');
 
-CommonMigrationFunctions::setTimeAndMemoryLimit();
+try {
+    CommonMigrationFunctions::setTimeAndMemoryLimit();
 
-$connections = CommonMigrationFunctions::getAllConnectionStrings();
-$manager = CommonMigrationFunctions::getGlobalDbConnectionManger();
-spl_autoload_register(array('Doctrine', 'autoload'));
+    $connections = CommonMigrationFunctions::getAllConnectionStrings();
+    $manager = CommonMigrationFunctions::getGlobalDbConnectionManger();
+    spl_autoload_register(array('Doctrine', 'autoload'));
 
+    $DMC = Doctrine_Manager::connection($connections['imbull'], 'doctrine');
 
-//$DMC = Doctrine_Manager::connection($connections['dsn'], 'doctrine_site');
-$DMC1 = Doctrine_Manager::connection($connections['imbull'], 'doctrine');
+    CommonMigrationFunctions::loadDoctrineModels();
 
-CommonMigrationFunctions::loadDoctrineModels();
+    $manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING, Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+    $manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+    $manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, true);
 
-$manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING, Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
-$manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
-$manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+    $migration = new Doctrine_Migration(realpath(APPLICATION_PATH.'/models/migrations_user'), $DMC );
+    $version = isset($argv[1]) ? $argv[1] : null ;
 
-$migration_user = new Doctrine_Migration(realpath(APPLICATION_PATH.'/models/migrations_user'), $DMC1 );
-$version = isset($argv[1]) ? $argv[1] : null ;
-$migration_user->migrate($version);
+    // echo $migration->getLatestVersion() . "\n";
+    // echo $migration->getNextVersion() . "\n";
+    // echo $migration->getNextMigrationClassVersion() . "\n";
+    // echo $migration->getCurrentVersion() . "\n";
 
-echo 'Database Migrated';
-die;
+    if($migration->getLatestVersion() > $migration->getCurrentVersion() ) {
+        # execute migrate()
+        $migration->migrate($version);
+        echo "Database has been Migrated successfully to v" . $migration->getCurrentVersion() . " \n\n";
+    } else {
+        echo "No migrations available. \n\n";
+    }
 
-/*echo "<p>Start generating migrations...</p>";
+    #close connection
+    $manager->closeConnection($DMC);
 
-
-Doctrine_Core::generateMigrationsFromDb(realpath('C:/wamp/www/migrations'));
-
-
-echo "<p>Migration classes added successfully.</p>";*/
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
