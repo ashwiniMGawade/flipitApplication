1d779d0f9ba56616de560eebbf3bb520824be91f
Kim Pellikaan <kim@web-flight.nl>
2015-08-25 12:17:21 +0200

    RemovKC-1 Removed just about everything to do with the doctrine migrations to start over.

--- cli/SiteDatabaseConfig/kortingscode_site.php
+++
@@ -1,6 +0,0 @@
-<?php
-		return array('dbname'   => 'kortingscode_site',
-	        'user'     => 'root',
-	    	'password' => 'root',
-	    	'host'     => 'localhost',
-	    	'driver'   => 'pdo_mysql');
--- cli/UserDatabaseConfig/kortingscode_user.php
+++
@@ -1,6 +0,0 @@
-<?php
-		return array('dbname'   => 'kortingscode_user',
-	        'user'     => 'root',
-	    	'password' => 'root',
-	    	'host'     => 'localhost',
-	    	'driver'   => 'pdo_mysql');
