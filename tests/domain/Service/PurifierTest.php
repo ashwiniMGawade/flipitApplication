a6325b67b33cb3ce93d1025ee99ec5722d60311e
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-31 16:01:42 +0530

    KC-6976 #comment Modifying the Purifier to handle an object

--- tests/domain/Service/PurifierTest.php
+++ tests/domain/Service/PurifierTest.php
@@ -11,15 +11,20 @@ class PurifierTest extends \Codeception\TestCase\Test
         $this->assertEquals('This is dummy input.', $purifiedData);
     }
 
+    public function testPurifierWithInputAsAnObject()
+    {
+        $purifiedData = ((new Purifier())->purify(new \stdClass()));
+        $this->assertEquals(new \stdClass(), $purifiedData);
+    }
+
     public function testPurifierWithInputAsAnArray()
     {
         $params = array(
             'id'    => 12,
             'name'  => 'This is dummy input.<script>Do some hacking.</script>',
-            'optional' => NULL
+            'optional' => new \stdClass()
         );
         $purifiedData = ((new Purifier())->purify($params));
-        $this->assertEquals(array('id'=>12, 'name'=>'This is dummy input.', 'optional' => NULL), $purifiedData);
+        $this->assertEquals(array('id'=>12, 'name'=>'This is dummy input.', 'optional' => new \stdClass()), $purifiedData);
     }
-
 }
