0b45c90bb109e162a8dc489233b0c0c1c1a7ebc5
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-08-31 13:26:19 +0530

    KC-6976 #comment Adding the Offer repository and interface

---
+++ core/Domain/Repository/OfferRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Core\Domain\Repository;
+
+interface OfferRepositoryInterface extends BaseRepositoryInterface
+{
+}
---
+++ core/Persistence/Database/Repository/OfferRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core\Persistence\Database\Repository;
+
+use \Core\Domain\Repository\OfferRepositoryInterface;
+
+class OfferRepository extends BaseRepository implements OfferRepositoryInterface
+{
+}
