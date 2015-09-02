5827c7e5909a74c38b739183e60e2fc71e8876fc
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-09-02 16:00:35 +0530

    KC-6977 #comment Refactoring the APiKeys Usecase and test, using the purifier and errors class in it

--- core/Domain/Usecase/Admin/GetApiKeysUsecase.php
+++ core/Domain/Usecase/Admin/GetApiKeysUsecase.php
@@ -1,19 +1,28 @@
 <?php
 namespace Core\Domain\Usecase\Admin;
 
+use Core\Domain\Adapter\PurifierInterface;
 use \Core\Domain\Repository\ApiKeyRepositoryInterface;
+use Core\Service\Errors\ErrorsInterface;
 
 class GetApiKeysUsecase
 {
     private $apiKeyRepository;
 
-    public function __construct(ApiKeyRepositoryInterface $apiKeyRepository)
+    public function __construct(ApiKeyRepositoryInterface $apiKeyRepository, PurifierInterface $htmlPurifier, ErrorsInterface $errors)
     {
         $this->apiKeyRepository = $apiKeyRepository;
+        $this->htmlPurifier = $htmlPurifier;
+        $this->errors = $errors;
     }
 
-    public function execute()
+    public function execute($conditions = array())
     {
-        return $this->apiKeyRepository->findBy('\Core\Domain\Entity\User\ApiKey', array('deleted' => 0));
+        if (!is_array($conditions)) {
+            $this->errors->setError('Invalid input, unable to find ApiKeys.');
+            return $this->errors;
+        }
+        $conditions = $this->htmlPurifier->purify($conditions);
+        return $this->apiKeyRepository->findBy('\Core\Domain\Entity\User\ApiKey', $conditions);
     }
 }
--- tests/domain/Usecase/Admin/GetApiKeysUsecaseTest.php
+++ tests/domain/Usecase/Admin/GetApiKeysUsecaseTest.php
@@ -1,24 +1,57 @@
 <?php
 namespace Usecase\Admin;
 
+use Core\Domain\Entity\User\ApiKey;
+use Core\Domain\Service\Purifier;
 use \Core\Domain\Usecase\Admin\GetApiKeysUsecase;
 use \Core\Domain\Repository\ApiKeyRepositoryInterface;
+use Core\Service\Errors;
 
 class GetApiKeysUsecaseTest extends \Codeception\TestCase\Test
 {
     protected $tester;
 
-    public function testGetApiKeysUsecaseTest()
+    public function testGetApiKeysUsecaseReturnsZeroWhenApiKeysDoesNotExist()
     {
+        $expectedClickCount = 0;
+        $apiKeyRepository = $this->createApiKeyRepositoryWithFindByMethodMock($expectedClickCount);
+        $viewCounts = (new GetApiKeysUsecase($apiKeyRepository, new Purifier(), new Errors()))->execute();
+        $this->assertEquals($expectedClickCount, $viewCounts);
+    }
+
+    public function testGetApiKeysUsecaseReturnsArrayWhenApiKeysExist()
+    {
+        $apiKey = new ApiKey();
+        $expectedResult = array($apiKey);
+        $apiKeyRepository = $this->createApiKeyRepositoryWithFindByMethodMock($expectedResult);
+        $viewCounts = (new GetApiKeysUsecase($apiKeyRepository, new Purifier(), new Errors()))->execute();
+        $this->assertEquals(count($expectedResult), count($viewCounts));
+    }
+
+    public function testGetOfferClickUsecaseReturnsErrorWhenParametersAreInvalid()
+    {
+        $conditions = 'invalid';
         $apiKeyRepository = $this->createApiKeyRepositoryMock();
-        $GetApiKeyListing = new GetApiKeysUsecase($apiKeyRepository);
-        $GetApiKeyListing->execute();
+        $errors = new Errors();
+        $errors->setError('Invalid input, unable to find ApiKeys.');
+        $result = (new GetApiKeysUsecase($apiKeyRepository, new Purifier(), new Errors()))->execute($conditions);
+        $this->assertInstanceOf('\Core\Service\Errors', $result);
+        $this->assertEquals($errors->getErrorsAll(), $result->getErrorsAll());
     }
 
     private function createApiKeyRepositoryMock()
     {
-        $apiKeyRepository = $this->getMockBuilder('\Core\Domain\Repository\ApiKeyRepositoryInterface')->getMock();
-        $apiKeyRepository->expects($this->once())->method('findBy');
-        return $apiKeyRepository;
+        $viewCountRepository = $this->getMock('\Core\Domain\Repository\ApiKeyRepositoryInterface');
+        return $viewCountRepository;
+    }
+
+    private function createApiKeyRepositoryWithFindByMethodMock($returns)
+    {
+        $viewCountRepository = $this->createApiKeyRepositoryMock();
+        $viewCountRepository->expects($this->once())
+            ->method('findBy')
+            ->with($this->equalTo('\Core\Domain\Entity\User\ApiKey'), $this->isType('array'))
+            ->willReturn($returns);
+        return $viewCountRepository;
     }
 }
