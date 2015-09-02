f446a1a1925f5a0da91403dbe32799396f226f85
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-09-02 16:26:59 +0530

    KC-6977 #comment Using setters instead of member variables

--- core/Domain/Usecase/Guest/AddOfferClickUsecase.php
+++ core/Domain/Usecase/Guest/AddOfferClickUsecase.php
@@ -16,12 +16,12 @@ class AddOfferClickUsecase
 
     public function execute(ViewCount $viewCount, Offer $offer, $clientIp)
     {
-        $viewCount->viewcount = $offer;
-        $viewCount->onClick = 1;
-        $viewCount->onLoad = 0;
-        $viewCount->IP = $clientIp;
-        $viewCount->created_at = new \DateTime('now');
-        $viewCount->updated_at = new \DateTime('now');
+        $viewCount->setViewcount($offer);
+        $viewCount->setOnClick(1);
+        $viewCount->setOnLoad(0);
+        $viewCount->setIP($clientIp);
+        $viewCount->setCreatedAt(new \DateTime('now'));
+        $viewCount->setUpdatedAt(new \DateTime('now'));
         return $this->viewCountRepository->save($viewCount);
     }
 }
