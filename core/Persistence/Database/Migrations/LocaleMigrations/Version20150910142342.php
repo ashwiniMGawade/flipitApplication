26a369be1f69eb0db55c455a411bef3e25a851d6
Ramesh Mhetre <ramesh@web-flight.nl>
2015-09-10 19:34:50 +0530

    KC-7056 #comment Added Landign pages table to the database

---
+++ core/Persistence/Database/Migrations/LocaleMigrations/Version20150910142342.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LocaleMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150910142342 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('landingPages');
+        $table->addColumn('id', 'integer', array('autoincrement' => true));
+        $table->addColumn('shopId', 'bigint');
+        $table->addColumn('title', 'string');
+        $table->addColumn('permalink', 'string');
+        $table->addColumn('subTitle', 'string', array('notnull' => 0));
+        $table->addColumn('metaTitle', 'string', array('notnull' => 0));
+        $table->addColumn('metaDescription', 'text', array('notnull' => 0));
+        $table->addColumn('content', 'blob', array('notnull' => 0));
+        $table->addColumn('status', 'boolean', array('default' => 1, 'length' => 1, 'comment' => '1-available ,0-used'));
+        $table->addColumn('offlineSince', 'datetime', array('length' => 12));
+        $table->addColumn('createdAt', 'datetime', array('length' => 12));
+        $table->addColumn('updatedAt', 'datetime', array('length' => 12));
+        $table->setPrimaryKey(array('id'));
+        $table->addForeignKeyConstraint('shop', array('shopId'), array('id'),  array("onDelete" => "CASCADE"), 'landingPages_shopId_shop_id');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('landingPages');
+
+    }
+}
