cb61f78e798def48006c8de6199180e8f132a251
Ramesh Mhetre <ramesh@web-flight.nl>
2015-09-11 17:07:03 +0530

    KC-7052 #comment GetLandingPageUsecase & testcase

---
+++ core/Persistence/Database/Repository/LandingPagesRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Core\Persistence\Database\Repository;
+
+use \Core\Domain\Repository\LandingPagesRepositoryInterface;
+
+class LandingPagesRepository extends BaseRepository implements LandingPagesRepositoryInterface
+{
+}
