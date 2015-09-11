625fa04385682cb1b340460c2940f686135df427
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-09-11 16:29:18 +0530

    KC-7052 #comment Adding a new landingpages controller, view and js file

---
+++ web/application/modules/admin/controllers/LandingpagesController.php
@@ -0,0 +1,68 @@
+<?php
+class Admin_LandingpagesController extends Zend_Controller_Action
+{
+    public function preDispatch()
+    {
+        if (!Auth_StaffAdapter::hasIdentity()) {
+            $referer = new Zend_Session_Namespace('referer');
+            $referer->refer = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+            $this->redirect('/admin/auth/index');
+        }
+        $this->view->controllerName = $this->getRequest()->getParam('controller');
+        $this->view->action = $this->getRequest()->getParam('action');
+        $sessionNamespace = new Zend_Session_Namespace();
+        if ($sessionNamespace->settings['rights']['administration']['rights'] != '1') {
+            $this->redirect('/admin');
+        }
+    }
+
+    public function init()
+    {
+    }
+
+    public function indexAction()
+    {
+    }
+
+    public function getAction()
+    {
+        $landingPages = array(
+            array(
+                'id' => '1',
+                'title' => 'Zalando Landing Page',
+                'shopName' => 'Zalando',
+                'permalink' => 'zalando-landing-page',
+                'shopOffersCount' => 123,
+                'shopCouponCount' => 21,
+                'shopClickoutCount' => 67243,
+                'status' => 1,
+                'offlineSince' => ''
+            ),
+            array(
+                'id' => '2',
+                'title' => 'Hema Landing Page',
+                'shopName' => 'Hema',
+                'permalink' => 'hema-landing-page',
+                'shopOffersCount' => 45,
+                'shopCouponCount' => 251,
+                'shopClickoutCount' => 57,
+                'status' => 1,
+                'offlineSince' => ''
+            ),
+            array(
+                'id' => '3',
+                'title' => 'Albelli Landing Page',
+                'shopName' => 'Albelli',
+                'permalink' => 'albelli-landing-page',
+                'shopOffersCount' => 787,
+                'shopCouponCount' => 3,
+                'shopClickoutCount' => 6547,
+                'status' => 0,
+                'offlineSince' => new \DateTime('now')
+            )
+        );
+        $response = \DataTable_Helper::createResponse(1, $landingPages, count($landingPages));
+        echo Zend_Json::encode($response);
+        exit;
+    }
+}
---
+++ web/public/js/back_end/landingPages.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+    var iStart = $.bbq.getState('iStart', true) || 0;
+    var iSortCol = $.bbq.getState('iSortCol', true) || 1;
+    var iSortDir = $.bbq.getState('iSortDir', true) || 'ASC';
+    if ($("table#LandingPagesListTbl").length) {
+        getLandingPages(iStart, iSortCol, iSortDir);
+    }
+});
+
+var LandingPagesListTbl = null;
+var click = false;
+function getLandingPages(iStart,iSortCol,iSortDir) {
+    $('#LandingPagesListTbl').addClass('widthTB');
+    LandingPagesListTbl = $("table#LandingPagesListTbl")
+    .dataTable({
+        "bLengthChange" : false,
+        "bFilter" : false,
+        "iDisplayStart" : iStart,
+        "iDisplayLength" : 20,
+        "oLanguage": {
+              "sInfo": "<b>_START_-_END_</b> of <b>_TOTAL_</b>"
+        },
+        "sPaginationType" : "bootstrap",
+        "sAjaxSource" : HOST_PATH+"admin/landingpages/get",
+        "aoColumns" : [{
+            "fnRender" : function(obj){
+                return id = obj.aData.id;
+            },
+            "bVisible": false,
+            "sType": 'numeric'
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.title != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.title +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.shopName != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.shopName +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.permalink != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.permalink +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.shopOffersCount != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.shopOffersCount +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.shopCouponCount != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.shopCouponCount +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.shopClickoutCount != null) {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.shopClickoutCount +'</a></p>';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.status != null) {
+                    var online = (obj.aData.status == 1) ? 'btn-primary' : '';
+                    var offline = (obj.aData.status == 0) ? 'btn-primary' : '';
+                    var html = "<div editId='" + obj.aData.id + "' class='btn-group'data-toggle='buttons-checkbox' style='margin-bottom:10px;'>"
+                        + "<button class='btn "+ online +"' onClick='changeStatus("+ obj.aData.id+",this,\"online\")'>"+__('Yes')+"</button>"
+                        + "<button class='btn "+ offline +"'onClick='changeStatus("+ obj.aData.id+",this,\"offline\")'>"+__('No')+"</button>"
+                        + "</div>";
+                    return html;
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                if(obj.aData.offlineSince != undefined && obj.aData.offlineSince != '') {
+                    return '<p editId=' + obj.aData.id + ' class="editId colorAsLink word-wrap-without-margin-offer"><a href="javascript:void(0)">' + obj.aData.offlineSince.date + '</a></p>';
+                } else {
+                    return '';
+                }
+            }
+        },
+        {
+            "fnRender" : function(obj) {
+                return "<a href='javascript:void(0);'" + "onclick='deleteLandingPage(" + obj.aData.id  + ")' >"
+                        + __('Delete') +
+                        "</a>";
+            },
+            "bSortable" : false
+        }]
+    });
+}
+//
+//function addApiKey()
+//{
+//    $.ajax({
+//        type : "GET",
+//        url : HOST_PATH+"admin/apikeys/create"
+//    }).done(function() {
+//        window.location = HOST_PATH + 'admin/apikeys';
+//    });
+//}
+//
+//function deleteApiKey(id) {
+//    bootbox.confirm("Are you sure you want to delete this Api Key?",'No','Yes',function(r) {
+//        if (!r) {
+//            return false;
+//        } else {
+//            addOverLay();
+//            $.ajax({
+//                type : "POST",
+//                url : HOST_PATH+"admin/apikeys/delete",
+//                data : "id="+id
+//            }).done(function(msg) {
+//                window.location = HOST_PATH + 'admin/apikeys';
+//            });
+//        }
+//    });
+//}
