c920bd353ce1aa2468e07bcd524f77fc673f1cb1
Ramesh Mhetre <ramesh@web-flight.nl>
2015-09-11 13:37:16 +0530

    KC-7052 #comment Created entity for this task

---
+++ core/Domain/Entity/LandingPages.php
@@ -0,0 +1,281 @@
+<?php
+namespace Core\Domain\Entity;
+use Doctrine\ORM\Mapping AS ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(
+ *     name="landingPages",
+ *     indexes={
+ *         @ORM\Index(name="landingPages_shopId_shop_id", columns={"shopId"})
+ *     }
+ * )
+ */
+class LandingPages
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", length=8)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Core\Domain\Entity\Shop", inversedBy="landingPages")
+     * @ORM\JoinColumn(name="shopId", referencedColumnName="id", unique=true)
+     */
+    protected $shop;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $permalink;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $subTitle;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $metaTitle;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $metaDescription;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $content;
+
+    /**
+     * @ORM\Column(type="integer", length=1)
+     */
+    protected $status = 1;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    protected $offlineSince;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=false)
+     */
+    protected $updatedAt;
+
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMetaDescription()
+    {
+        return $this->metaDescription;
+    }
+
+    /**
+     * @param mixed $metaDescription
+     */
+    public function setMetaDescription($metaDescription)
+    {
+        $this->metaDescription = $metaDescription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMetaTitle()
+    {
+        return $this->metaTitle;
+    }
+
+    /**
+     * @param mixed $metaTitle
+     */
+    public function setMetaTitle($metaTitle)
+    {
+        $this->metaTitle = $metaTitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOfflineSince()
+    {
+        return $this->offlineSince;
+    }
+
+    /**
+     * @param mixed $offlineSince
+     */
+    public function setOfflineSince($offlineSince)
+    {
+        $this->offlineSince = $offlineSince;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPermalink()
+    {
+        return $this->permalink;
+    }
+
+    /**
+     * @param mixed $permalink
+     */
+    public function setPermalink($permalink)
+    {
+        $this->permalink = $permalink;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShop()
+    {
+        return $this->shop;
+    }
+
+    /**
+     * @param mixed $shop
+     */
+    public function setShop($shop)
+    {
+        $this->shop = $shop;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubTitle()
+    {
+        return $this->subTitle;
+    }
+
+    /**
+     * @param mixed $subTitle
+     */
+    public function setSubTitle($subTitle)
+    {
+        $this->subTitle = $subTitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param mixed $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function __set($property, $value)
+    {
+        $this->$property = $value;
+    }
+}
