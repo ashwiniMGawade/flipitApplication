f4e90d1b117ced695404fb792e8569cf65284b75
Ramesh Mhetre <ramesh@web-flight.nl>
2015-09-23 12:10:10 +0530

    KC-7154 #comment Moved VWO script underneath the head tag for all previously implemented pages

--- web/application/controllers/StoreController.php
+++ web/application/controllers/StoreController.php
@@ -7,11 +7,7 @@ class StoreController extends Zend_Controller_Action
         $module   = strtolower($this->getRequest()->getParam('lang'));
         $controller = strtolower($this->getRequest()->getControllerName());
         $action     = strtolower($this->getRequest()->getActionName());
-        if (
-            file_exists(
-                APPLICATION_PATH . '/modules/' . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml"
-            )
-        ) {
+        if (file_exists(APPLICATION_PATH . '/modules/' . $module . '/views/scripts/' . $controller . '/' . $action . ".phtml")) {
             $this->view->setScriptPath(APPLICATION_PATH . '/modules/'  . $module . '/views/scripts');
         } else {
             $this->view->setScriptPath(APPLICATION_PATH . '/views/scripts');
@@ -497,9 +493,6 @@ class StoreController extends Zend_Controller_Action
         switch (LOCALE) {
             case 'es':
                 $shopPermalinks = array('kiabi', 'rakuten-es', 'groupalia');
-                if( $shopPermalink === 'kiabi') {
-                    $this->view->displayVWOScriptOnTop = 1;
-                }
                 break;
             case 'sg':
                 $shopPermalinks = array('zalora', 'foodpanda', 'groupon');
