4653234650e9e87fa17de5e73937aa70557e18c6
Ramesh Mhetre <ramesh@web-flight.nl>
2015-10-07 15:51:51 +0530

    KC-7122 #comment HTML Lang setting implemented on front end

--- core/Domain/Factory/SystemFactory.php
+++ core/Domain/Factory/SystemFactory.php
@@ -3,8 +3,10 @@ namespace Core\Domain\Factory;
 
 use \Core\Domain\Usecase\System\GetApiKeyUsecase;
 use \Core\Domain\Usecase\System\DeactivateSleepingVisitors;
+use \Core\Domain\Usecase\System\GetSettingUsecase;
 use \Core\Persistence\Factory\RepositoryFactory;
 use \Core\Domain\Service\Purifier;
+use \Core\Service\Errors;
 
 class SystemFactory
 {
@@ -16,5 +18,10 @@ class SystemFactory
     public static function getApiKey()
     {
         return new GetApiKeyUsecase(RepositoryFactory::apiKeys(), new Purifier());
+    }
+
+    public static function getSetting()
+    {
+        return new GetSettingUsecase(RepositoryFactory::settings(), new Purifier(), new Errors());
     }
 }
