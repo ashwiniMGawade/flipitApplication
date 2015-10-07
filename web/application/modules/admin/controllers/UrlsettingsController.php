dce40bfb6526d792ccdd6b0ca657ed2b2568d9d7
Pooja Khorjuvekar <pooja@web-flight.nl>
2015-10-07 19:08:28 +0530

    KC-7127 #comment Adding the create, edit, delete actions

--- web/application/modules/admin/controllers/UrlsettingsController.php
+++ web/application/modules/admin/controllers/UrlsettingsController.php
@@ -46,137 +46,64 @@ class Admin_UrlsettingsController extends Application_Admin_BaseController
 
     public function createAction()
     {
-        $conditions = array(
-            'deleted' => 0,
-            'status' => 1
-        );
-
-        $order = array(
-            'name' => 'ASC'
-        );
-        $this->view->shops = \KC\Repository\Shop::getOfferShopList();
-
         if ($this->_request->isPost()) {
-            $landingPage = AdminFactory::createLandingPage()->execute();
-            $shopId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('selectedShop')));
-            if ($shopId <= 0) {
-                $this->view->landingPage = $this->getAllParams();
-                $this->setFlashMessage('error', 'Invalid Shop');
+            $urlSetting = AdminFactory::createURLSettings()->execute();
+
+            $parameters['status'] = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('status')));
+            $parameters['url'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('url'));
+
+            $result = AdminFactory::addURLSetting()->execute($urlSetting, $parameters);
+
+            if ($result instanceof Errors) {
+                $this->view->urlSetting = $this->getAllParams();
+                $errors = $result->getErrorsAll();
+                $this->setFlashMessage('error', $errors);
             } else {
-                $shop = AdminFactory::getShop()->execute($shopId);
-
-                $conditions = array(
-                    'shop' => $shop
-                );
-                $landingPagesForShop = AdminFactory::getLandingPages()->execute($conditions);
-
-                if (count($landingPagesForShop) > 0) {
-                    $this->view->landingPage = $this->getAllParams();
-                    $this->setFlashMessage('error', 'Landing Page already exists for '.$shop->getName().'. Please select a different shop.');
-                } else {
-                    $parameters['status'] = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('status')));
-                    $parameters['title'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('title'));
-                    $parameters['permalink'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('permalink'));
-                    $parameters['subTitle'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('subTitle'));
-                    $parameters['metaTitle'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('overwriteTitle'));
-                    $parameters['metaDescription'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('metaDescription'));
-                    $parameters['content'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('pageContent'));
-                    $parameters['shop'] = $shop;
-
-                    $result = AdminFactory::addLandingPage()->execute($landingPage, $parameters);
-
-                    if ($result instanceof Errors) {
-                        $this->view->landingPage = $this->getAllParams();
-                        $errors = $result->getErrorsAll();
-                        $this->setFlashMessage('error', $errors);
-                    } else {
-                        $this->setFlashMessage('success', 'Landing Page has been added successfully');
-                        $this->redirect(HTTP_PATH.'admin/landingpages');
-                    }
-                }
+                $this->setFlashMessage('success', 'VWO Tag has been added successfully');
+                $this->redirect(HTTP_PATH.'admin/urlsettings');
             }
         }
     }
 
     public function editAction()
     {
-        $conditions = array(
-            'deleted' => 0,
-            'status' => 1
-        );
-
-        $order = array(
-            'name' => 'ASC'
-        );
-        $this->view->shops = \KC\Repository\Shop::getOfferShopList();
-
-        $landingPageId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('id')));
+        $urlSettingId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('id')));
         $this->view->qstring = $_SERVER['QUERY_STRING'];
-        if ($landingPageId <= 0) {
-            $this->setFlashMessage('error', 'Invalid Landing Page Id provided.');
-            $this->redirect(HTTP_PATH.'admin/landingpages');
+        if ($urlSettingId <= 0) {
+            $this->setFlashMessage('error', 'Invalid Id provided.');
+            $this->redirect(HTTP_PATH.'admin/urlsettings');
         }
-        $result = AdminFactory::getLandingPage()->execute(array('id' => $landingPageId));
+        $result = AdminFactory::getURLSetting()->execute(array('id' => $urlSettingId));
         if ($result instanceof Errors) {
             $errors = $result->getErrorsAll();
             $this->setFlashMessage('error', $errors);
-            $this->redirect(HTTP_PATH.'admin/landingpages');
+            $this->redirect(HTTP_PATH.'admin/urlsettings');
         } else {
-            $landingPage = $result;
-            $this->view->id = $landingPageId;
-            $permalink = $landingPage->getPermalink();
-            $shop = $landingPage->getShop();
-            $shopId = $shop->getId();
+            $urlSetting = $result;
+            $this->view->id = $urlSettingId;
+            $url = $urlSetting->getUrl();
 
-            $landingPageInfo = array(
-                'id' => $landingPage->getId(),
-                'status' => $landingPage->getStatus(),
-                'title' => $landingPage->getTitle(),
-                'subTitle' => $landingPage->getSubTitle(),
-                'permalink' => $landingPage->getPermalink(),
-                'pageContent' => $landingPage->getContent(),
-                'metaDescription' => $landingPage->getMetaDescription(),
-                'overwriteTitle' => $landingPage->getMetaTitle(),
-                'selectedShop' => $shopId,
+            $urlSettingInfo = array(
+                'id' => $urlSetting->getId(),
+                'status' => $urlSetting->getStatus(),
+                'url' => $urlSetting->getUrl()
             );
-            $this->view->landingPage = $landingPageInfo;
+            $this->view->urlSetting = $urlSettingInfo;
 
             if ($this->_request->isPost()) {
-                $shopId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('selectedShop')));
-                if ($shopId <= 0) {
-                    $this->view->landingPage = $this->getAllParams();
-                    $this->setFlashMessage('error', 'Invalid Shop');
+                $parameters['status'] = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('status')));
+                $parameters['url'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('url'));
+
+                $result = AdminFactory::updateURLSetting()->execute($urlSetting, $parameters);
+
+                if ($result instanceof Errors) {
+                    $this->view->urlSetting = $this->getAllParams();
+                    $errors = $result->getErrorsAll();
+                    $this->setFlashMessage('error', $errors);
                 } else {
-                    $shop = AdminFactory::getShop()->execute($shopId);
-                    $conditions = array(
-                        'shop' => $shop
-                    );
-                    $landingPagesForShop = AdminFactory::getLandingPages()->execute($conditions);
-                    if ((count($landingPagesForShop) == 1 && $landingPagesForShop[0]->getId() != $landingPageId) || count($landingPagesForShop) > 1) {
-                        $this->view->landingPage = $this->getAllParams();
-                        $this->setFlashMessage('error', 'Landing Page already exists for '.$shop->getName().'. Please select a different shop.');
-                    } else {
-                        $parameters['status'] = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('status')));
-                        $parameters['title'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('title'));
-                        $parameters['permalink'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('permalink'));
-                        $parameters['subTitle'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('subTitle'));
-                        $parameters['metaTitle'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('overwriteTitle'));
-                        $parameters['metaDescription'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('metaDescription'));
-                        $parameters['content'] = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('pageContent'));
-                        $parameters['shop'] = $shop;
-
-                        $result = AdminFactory::updateLandingPage()->execute($landingPage, $parameters);
-
-                        if ($result instanceof Errors) {
-                            $this->view->landingPage = $this->getAllParams();
-                            $errors = $result->getErrorsAll();
-                            $this->setFlashMessage('error', $errors);
-                        } else {
-                            self::updateVarnish($permalink);
-                            $this->setFlashMessage('success', 'Landing Page has been updated successfully');
-                            $this->redirect(HTTP_PATH.'admin/landingpages');
-                        }
-                    }
+                    self::updateVarnish($url);
+                    $this->setFlashMessage('success', 'VWO Tag has been added successfully');
+                    $this->redirect(HTTP_PATH.'admin/urlsettings');
                 }
             }
         }
@@ -184,48 +111,44 @@ class Admin_UrlsettingsController extends Application_Admin_BaseController
 
     public function deleteAction()
     {
-        $landingPageId = intval($this->getRequest()->getParam('id'));
-        if (intval($landingPageId) < 1) {
-            $this->setFlashMessage('error', 'Invalid page id provided.');
-            die;
+        $urlSettingId = intval($this->getRequest()->getParam('id'));
+        if (intval($urlSettingId) < 1) {
+            $this->setFlashMessage('error', 'Invalid id provided.');
+            exit;
         }
 
-        $result = AdminFactory::getLandingPage()->execute(array('id' => $landingPageId));
+        $result = AdminFactory::getURLSetting()->execute(array('id' => $urlSettingId));
         if ($result instanceof Errors) {
             $errors = $result->getErrorsAll();
             $this->setFlashMessage('error', $errors);
-            die;
+            exit;
         }
-        AdminFactory::deleteLandingPage()->execute($result);
-        self::updateVarnish($result->getPermalink());
-        $this->setFlashMessage('success', 'Landing page deleted successfully.');
-        die;
+        AdminFactory::deleteURLSetting()->execute($result);
+        self::updateVarnish($result->getUrl());
+        $this->setFlashMessage('success', 'VWO Tag deleted successfully.');
+        exit;
     }
 
-    public function updateVarnish($permalink)
+    public function updateVarnish($url)
     {
         $varnishObj = new \KC\Repository\Varnish();
-        $varnishObj->addUrl(HTTP_PATH_FRONTEND.'glp/'.$permalink);
+        $varnishObj->addUrl(HTTP_PATH_FRONTEND.$url);
     }
 
-    public function validatepermalinkAction()
+    public function validateurlAction()
     {
         $isValid = false;
-        $permalink = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('permalink'));
-        $landingPageId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('editId')));
-
-        $pattern = array("/&amp;/", "/[\,+@#$%'&*!;&\"<>\^()]+/", '/\s/', "/-{2,}/");
-        $replaceWith = array("", "", "-", "-");
-        $urlString = preg_replace($pattern, $replaceWith, trim($permalink));
-        $permalink = strtolower($urlString);
+        $url = FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('url'));
+        $urlSettingId = intval(FrontEnd_Helper_viewHelper::sanitize($this->getRequest()->getParam('editId')));
 
         $conditions = array(
-            'permalink' => $permalink
+            'url' => $url
         );
-        $landingPages = AdminFactory::getLandingPages()->execute($conditions);
-        if (count($landingPages) == 0) {
+        $urlSettings = AdminFactory::getURLSettings()->execute($conditions);
+
+        if (count($urlSettings) == 0) {
             $isValid = true;
-        } elseif (count($landingPages) == 1 && $landingPages[0]->getId() == $landingPageId) {
+        } elseif (count($urlSettings) == 1 && $urlSettings[0]->getId() == $urlSettingId) {
             $isValid = true;
         }
         echo Zend_Json::encode($isValid);
