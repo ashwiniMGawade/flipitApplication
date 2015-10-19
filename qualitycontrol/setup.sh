780bc8bd85cf7745eccac226181fb34997ba0c7e
Kim Pellikaan <kim@web-flight.nl>
2015-10-19 16:42:07 +0200

    Added condition to ignore the precommit hooks while using scrutiniser.

--- qualitycontrol/setup.sh
+++ qualitycontrol/setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-cp qualitycontrol/pre-commit qualitycontrol/commit-msg ../.git/modules/flipit_application/hooks
-chmod +x ../.git/modules/flipit_application/hooks/pre-commit ../.git/modules/flipit_application/hooks/commit-msg
+if [ "$SCRUTINIZER" ]
+    then
+    cp qualitycontrol/pre-commit qualitycontrol/commit-msg ../.git/modules/flipit_application/hooks
+    chmod +x ../.git/modules/flipit_application/hooks/pre-commit ../.git/modules/flipit_application/hooks/commit-msg
+fi
