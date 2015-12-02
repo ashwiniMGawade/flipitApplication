39413a38d35489b15ae951d30089d24301fb580f
ashwini_webflight <ashwini@web-flight.nl>
2015-12-02 13:25:06 +0530

    KC-7477 #comment created migration for the campaigns table.

---
+++ core/Persistence/Database/Migrations/LocaleMigrations/Version20151202065354.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace LocaleMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20151202065354 extends AbstractMigration
+{
+    /*
+    * @param Schema $schema
+    */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('newsletterCampaigns');
+        $table->addColumn('id', 'integer', array('autoincrement' => true));
+        $table->addColumn('campaignName', 'string', array('notnull' => 0));
+        $table->addColumn('senderName', 'string', array('notnull' => 0));
+        $table->addColumn('senderEmail', 'string', array('notnull' => 0));
+        $table->addColumn('header', 'text', array('notnull' => 0));
+        $table->addColumn('headerBanner', 'string', array('notnull' => 0));
+        $table->addColumn('headerBannerURL', 'string', array('notnull' => 0));
+        $table->addColumn('footer', 'text', array('notnull' => 0));
+        $table->addColumn('footerBanner', 'string', array('notnull' => 0));
+        $table->addColumn('footerBannerURL', 'string', array('notnull' => 0));
+        $table->addColumn('offerPartOneTitle', 'string', array('notnull' => 0));
+        $table->addColumn('offerPartTwoTitle', 'string', array('notnull' => 0));
+        $table->addColumn('scheduledStatus', 'boolean', array('default' => 0, 'length' => 1, 'comment' => '1-Scheduled, 0-Not Scheduled'));
+        $table->addColumn('scheduledTime', 'datetime', array('notnull' => 0, 'length' => 12));
+        $table->addColumn('isStrated', 'boolean', array('default' => 0, 'length' => 1, 'comment' => '1-Stated, 0-Not Started'));
+        $table->addColumn('newsletterSentTime', 'datetime', array('notnull' => 0, 'length' => 12));
+        $table->addColumn('receipientCount', 'integer',  array('notnull' => 0));
+        $table->addColumn('createdAt', 'datetime', array('notnull' => 1, 'length' => 12));
+        $table->addColumn('updatedAt', 'datetime', array('length' => 12));
+        $table->setPrimaryKey(array('id'));
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('newsletterCampaigns');
+    }
+}
