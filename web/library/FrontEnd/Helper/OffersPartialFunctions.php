5471f83db6565accbddb4ecedb769002b42b2048
Ramesh Mhetre <ramesh@web-flight.nl>
2015-12-04 12:59:30 +0530

    KC-7517 #comemnt Made exisitng splash working

--- web/library/FrontEnd/Helper/OffersPartialFunctions.php
+++ web/library/FrontEnd/Helper/OffersPartialFunctions.php
@@ -709,7 +709,7 @@ class FrontEnd_Helper_OffersPartialFunctions
                 $offerCount = (count($splashImages) < $offerCount) ? 1 : $offerCount;
                 $splashImage = true === array_key_exists($offerCount, $splashImages) ? $splashImages[$offerCount]->getImage() : 'slide_img1.jpg';
                 $couponHtml .= '<div class="slide">
-                        <img src="'.PUBLIC_PATH.'images/front_end/trading_coupon/'.$splashImage.'" alt="'.$offer->getTitle().'">
+                        <img src="'.PUBLIC_PATH.'images/upload/splash/'.$splashImage.'" alt="'.$offer->getTitle().'">
                         <div class="caption">
                             <div class="img-holder">
                                 <a href="'.$clickUrl.'"><img src="'.$logoPath.$offer->getShopOffers()->getLogo()->path.'thum_big_'.$offer->getShopOffers()->getLogo()->name.'" alt="'.$offer->getShopOffers()->getName().'"></a>
