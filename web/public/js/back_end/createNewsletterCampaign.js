aa944ce33a2ed2d02c30c603a462cadd3617b8a5
ashwini_webflight <ashwini@web-flight.nl>
2015-12-07 12:26:55 +0530

    KC-7435 #comment added client side validation while creating new campaign

--- web/public/js/back_end/createNewsletterCampaign.js
+++ web/public/js/back_end/createNewsletterCampaign.js
@@ -53,4 +53,266 @@ $(function(){
             }
         });
 
+        /**
+         * validRules object contain all the messages that are visible when an elment
+         * value is valid
+         *
+         * structure to define a message for element: key is to be element name and Value is
+         * message
+         */
+        var validRules = {
+            "campaignHeader" : __("Campaign header looks great"),
+            "campaignFooter" : __("Campaign foorer looks great"),
+            "senderEmail" : __("Email address looks great"),
+            "headerBannerURL" : __("Campaign header banner URL looks great"),
+            "footerBannerURL" : __("Campaign footer banner URL looks great")
+        };
+
+        /**
+         * focusRules oject contain all the messages that are visible on focus of an
+         * elelement
+         *
+         * structure to define a message for element : key is to be element name and Value is
+         * message
+         */
+        var focusRules = {
+            "campaignHeader" : __("Please enter valid Campaign header"),
+            "campaignFooter" : __("Please enter valid Campaign foorert"),
+            "senderEmail" : __("Please enter valid Email address"),
+            "headerBannerURL" : __("Please enter valid Campaign header banner URL"),
+            "footerBannerURL" : __("Please enter valid Campaign footer banner URL")
+        };
+
+
+
+    var invalidForm = {} ;
+        var errorBy = "" ;
+
+        $.validator.setDefaults({
+            onkeyup : false,
+            onfocusout : function(element) {
+                $(element).valid();
+            }
+
+        });
+
+        function resetBorders(el)
+        {
+            $(el).each(function(i,o){
+                $(o).parent('div')
+                    .removeClass("error success")
+                    .prev("div").removeClass('focus error success') ;
+
+            });
+        }
+
+        var init = function ()
+        {
+            $("form").submit(function(e){
+                // e.preventDefault();
+                if (! jQuery.isEmptyObject(invalidForm) ) {
+                    for (var i in invalidForm) {
+                        if (invalidForm[i]) {
+                            $('#saveNewsletterCampaign').removeAttr('disabled');
+                            return false;
+                        }
+                    }
+                }
+            });
+
+            //function call to validate new category
+            validateWizardForm();
+        }
+
+        var validateWizardForm = function (){
+            validatorForwizardForm = $("form#wizardform").validate(
+                {
+                    errorClass : 'error',
+                    validClass : 'success',
+                    errorElement : 'span',
+                    ignore: ".ignore, :hidden",
+                    afterReset  : resetBorders,
+                    errorPlacement : function(error, element) {
+                        element.parent("div").prev("div")
+                            .html(error);
+                    },
+                    rules : {
+                        "senderEmail": {
+                            required : true,
+                            email    : true
+                        },
+                        "campaignHeader": {
+                            minlength : 10
+                        },
+                        "campaignFooter": {
+                            minlength : 10
+                        },
+                        "headerBannerURL": {
+                            url : true
+                        },
+                        "footerBannerURL": {
+                            url : true
+                        }
+                    },
+                    messages : {
+                        "senderEmail" : {
+                            required : __("Please enter your email address"),
+                            email : __("Please enter valid email address")
+                        },
+                        "campaignHeader": {
+                            minlength : __("Please enter atleast 10 characters")
+                        },
+                        "campaignFooter"  : {
+                            minlength : __("Please enter atleast 10 characters")
+                        },
+                        "headerBannerURL": {
+                            url : __("Please enter valid URL")
+                        },
+                        "footerBannerURL"  : {
+                            url : __("Please enter valid URL")
+                        }
+                    },
+                    onfocusin : function(element) {
+                        // display hint messages when an element got focus
+                        if (!$(element).parent('div').prev("div")
+                                .hasClass('success')) {
+
+                            var label = this.errorsFor(element);
+
+                            if( $( label ).attr('hasError')  ) {
+                                if($( label ).attr('remote-validated') != "true") {
+                                    this.showLabel(element, focusRules[element.name]);
+
+                                    $(element).parent('div').removeClass(
+                                        this.settings.errorClass)
+                                        .removeClass(
+                                        this.settings.validClass)
+                                        .prev("div")
+                                        .addClass('focus')
+                                        .removeClass(
+                                        this.settings.errorClass)
+                                        .removeClass(
+                                        this.settings.validClass);
+                                }
+                            } else {
+                                this.showLabel(element, focusRules[element.name]);
+                                $(element).parent('div').removeClass(
+                                    this.settings.errorClass)
+                                    .removeClass(
+                                    this.settings.validClass)
+                                    .prev("div")
+                                    .addClass('focus')
+                                    .removeClass(
+                                    this.settings.errorClass)
+                                    .removeClass(
+                                    this.settings.validClass);
+                            }
+                        }
+                    },
+
+                    highlight : function(element, errorClass, validClass) {
+                        // highlight borders in case of error
+                        $(element).parent('div')
+                            .removeClass(validClass)
+                            .addClass(errorClass).prev(
+                            "div").removeClass(
+                            validClass)
+                            .addClass(errorClass);
+
+                        $('span.help-inline', $(element).parent('div')
+                            .prev('div')).removeClass(validClass) ;
+
+
+
+                    },
+                    unhighlight : function(element, errorClass, validClass) {
+                        // check for ignored elemnets and highlight borders when succeed
+                        if(! $(element).hasClass("ignore")) {
+                            $(element).parent('div')
+                                .removeClass(errorClass)
+                                .addClass(validClass).prev(
+                                "div").addClass(
+                                validClass)
+                                .removeClass(errorClass);
+                            $(
+                                'span.help-inline',
+                                $(element).parent('div')
+                                    .prev('div')).text(
+                                validRules[element.name]) ;
+                        } else {
+                            // check to display errors for ignored elements or not
+                            var showError = false ;
+                            //
+                            switch( element.nodeName.toLowerCase() ) {
+                                case 'input':
+                                    if ( this.checkable(element) ) {
+
+                                        showError = this.getLength(element.value, element) > 0;
+
+                                    } else if($.trim(element.value).length > 0) {
+
+                                        showError =  true ;
+
+                                    } else {
+
+                                        showError = false ;
+                                    }
+
+                                    break ;
+                                default:
+                                    var val = $(element).val();
+                                    showError =  $.trim(val).length > 0;
+                            }
+                            if(! showError ) {
+                                // hide errors message and remove highlighted borders
+                                $(
+                                    'span.help-inline',
+                                    $(element).parent('div')
+                                        .prev('div')).hide();
+
+                                $(element).parent('div')
+                                    .removeClass(errorClass)
+                                    .removeClass(validClass)
+                                    .prev("div")
+                                    .removeClass(errorClass)
+                                    .removeClass(validClass) ;
+                            } else {
+                                // show errors message and  highlight borders
+
+                                // display green border and message
+                                $(element).parent('div')
+                                    .removeClass(errorClass)
+                                    .removeClass(validClass)
+                                    .removeClass("focus")
+                                    .prev("div")
+                                    .removeClass("focus")
+                                    .removeClass(errorClass)
+                                    .removeClass(validClass) ;
+                            }
+                        }
+
+                    },
+                    success: function(label , element) {
+                        $(element).parent('div')
+                            .removeClass(this.errorClass)
+                            .addClass(this.validClass).prev(
+                            "div").addClass(
+                            this.validClass)
+                            .removeClass(this.errorClass)
+                            .removeClass("focus");
+                        $(label).append( validRules[element.name] ) ;
+                        label.addClass('valid') ;
+                    }
+                });
+        }
+
+        init();
+        $('form#wizardform').submit(function () {
+            if ($("form#wizardform").valid()) {
+                $('#saveNewsletterCampaign').attr('disabled', "disabled");
+                return true;
+            } else {
+                return false;
+            }
+        });
     });
