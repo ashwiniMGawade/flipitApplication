fd066776b30dd729e05908961d92ee4ce0b7e8d9
ashwini_webflight <ashwini@web-flight.nl>
2015-12-29 11:25:05 +0530

    KC-7507 #comment added migrations and modified test casess to store the scheduletime in timestamp

---
+++ core/Persistence/Database/Migrations/LocaleMigrations/Version20151229063217.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LocaleMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+
+class Version20151229063217 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->connection->executeQuery("alter table newsletterCampaigns  CHANGE  `scheduledTime`  `scheduledTime` BIGINT NULL DEFAULT NULL");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->connection->executeQuery("alter table newsletterCampaigns CHANGE  `scheduledTime`  `scheduledTime` DATETIME NULL DEFAULT NULL");
+
+    }
+}
