0740c58ba58c8f67bd62f3a4c764552e5470109d
Ramesh Mhetre <ramesh@web-flight.nl>
2016-02-05 17:48:28 +0530

    KC-7740 #comment Fixed Brand shop functionality & expired sale on shop page issues in release

--- web/application/modules/admin/controllers/BrandingController.php
+++ web/application/modules/admin/controllers/BrandingController.php
@@ -26,4 +26,24 @@ class Admin_BrandingController extends Application_Admin_BaseController
         $redirectUrl = $this->_helper->branding->stop();
         $this->_redirect( $redirectUrl );
     }
+
+    public function startGlpAction()
+    {
+        $session = new Zend_Session_Namespace('BrandingGlp');
+        $httpScheme = FrontEnd_Helper_viewHelper::getServerNameScheme();
+        $session->saveUrl = 'http://'.$httpScheme.'.kortingscode.nl/';
+        $redirectUrl = $this->_helper->branding->startGLP();
+        $this->redirect($redirectUrl);
+    }
+
+    public function saveGlpAction()
+    {
+        $this->redirect($this->_helper->branding->saveGLP());
+    }
+
+    public function stopGlpAction()
+    {
+        $redirectUrl = $this->_helper->branding->stopGLP();
+        $this->redirect($redirectUrl);
+    }
 }
